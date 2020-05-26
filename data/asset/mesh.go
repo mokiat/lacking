120819bf8c76ef56e2c12593508d43ee180ceaaf
Momchil Atanasov <momchil.dev@gmail.com>
2020-05-26 22:58:22 +0300

    enhance mesh asset

--- data/asset/mesh.go
+++ data/asset/mesh.go
@@ -2,22 +2,45 @@ package asset
 
 import "io"
 
+const UnspecifiedOffset = int16(-1)
+
 type Mesh struct {
 	VertexData     []byte
-	VertexStride   uint8
-	CoordOffset    uint8
-	NormalOffset   uint8
-	TexCoordOffset uint8
+	VertexStride   int16
+	CoordOffset    int16
+	NormalOffset   int16
+	TangentOffset  int16
+	TexCoordOffset int16
+	ColorOffset    int16
 	IndexData      []byte
 	SubMeshes      []SubMesh
 }
 
 type SubMesh struct {
-	Name           string
-	IndexOffset    uint32
-	IndexCount     uint32
-	DiffuseTexture string
+	Name             string
+	Primitive        Primitive
+	IndexOffset      uint32
+	IndexCount       uint32
+	MaterialType     string
+	Color            [4]float32
+	ColorTexture     string
+	Roughness        float32
+	RoughnessTexture string
+	NormalScale      float32
+	NormalTexture    string
 }
+
+type Primitive uint8
+
+const (
+	PrimitivePoints Primitive = iota
+	PrimitiveLines
+	PrimitiveLineStrip
+	PrimitiveLineLoop
+	PrimitiveTriangles
+	PrimitiveTriangleStrip
+	PrimitiveTriangleFan
+)
 
 func EncodeMesh(out io.Writer, mesh *Mesh) error {
 	return Encode(out, mesh)
