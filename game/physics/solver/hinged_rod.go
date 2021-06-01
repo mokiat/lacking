3d5b27f84975aa3d978e82b39e15fcd1d62c7c5f
Momchil Atanasov <momchil.dev@gmail.com>
2021-06-02 00:19:58 +0300

    wip: add hinged rod constraint

---
+++ game/physics/solver/hinged_rod.go
@@ -0,0 +1,91 @@
+package solver
+
+import (
+	"github.com/mokiat/gomath/sprec"
+	"github.com/mokiat/lacking/game/physics"
+)
+
+var _ physics.DBConstraintSolver = (*HingedRod)(nil)
+
+func NewHingedRod() *HingedRod {
+	result := &HingedRod{
+		length: 1.0,
+	}
+	result.DBJacobianConstraintSolver = physics.NewDBJacobianConstraintSolver(result.calculate)
+	return result
+}
+
+type HingedRod struct {
+	*physics.DBJacobianConstraintSolver
+
+	primaryAnchor   sprec.Vec3
+	secondaryAnchor sprec.Vec3
+	length          float32
+}
+
+func (r *HingedRod) PrimaryAnchor() sprec.Vec3 {
+	return r.primaryAnchor
+}
+
+func (r *HingedRod) SetPrimaryAnchor(anchor sprec.Vec3) *HingedRod {
+	r.primaryAnchor = anchor
+	return r
+}
+
+func (r *HingedRod) SecondaryAnchor() sprec.Vec3 {
+	return r.secondaryAnchor
+}
+
+func (r *HingedRod) SetSecondaryAnchor(anchor sprec.Vec3) *HingedRod {
+	r.secondaryAnchor = anchor
+	return r
+}
+
+func (r *HingedRod) Length() float32 {
+	return r.length
+}
+
+func (r *HingedRod) SetLength(length float32) *HingedRod {
+	r.length = length
+	return r
+}
+
+func (r *HingedRod) calculate(ctx physics.DBSolverContext) (physics.PairJacobian, float32) {
+	firstRadiusWS := sprec.QuatVec3Rotation(ctx.Primary.Orientation(), r.primaryAnchor)
+	secondRadiusWS := sprec.QuatVec3Rotation(ctx.Secondary.Orientation(), r.secondaryAnchor)
+	firstAnchorWS := sprec.Vec3Sum(ctx.Primary.Position(), firstRadiusWS)
+	secondAnchorWS := sprec.Vec3Sum(ctx.Secondary.Position(), secondRadiusWS)
+	deltaPosition := sprec.Vec3Diff(secondAnchorWS, firstAnchorWS)
+	normal := sprec.BasisXVec3()
+	if deltaPosition.SqrLength() > sqrEpsilon {
+		normal = sprec.UnitVec3(deltaPosition)
+	}
+
+	return physics.PairJacobian{
+			Primary: physics.Jacobian{
+				SlopeVelocity: sprec.NewVec3(
+					-normal.X,
+					-normal.Y,
+					-normal.Z,
+				),
+				SlopeAngularVelocity: sprec.NewVec3(
+					-(normal.Z*firstRadiusWS.Y - normal.Y*firstRadiusWS.Z),
+					-(normal.X*firstRadiusWS.Z - normal.Z*firstRadiusWS.X),
+					-(normal.Y*firstRadiusWS.X - normal.X*firstRadiusWS.Y),
+				),
+			},
+			Secondary: physics.Jacobian{
+				SlopeVelocity: sprec.NewVec3(
+					normal.X,
+					normal.Y,
+					normal.Z,
+				),
+				SlopeAngularVelocity: sprec.NewVec3(
+					normal.Z*secondRadiusWS.Y-normal.Y*secondRadiusWS.Z,
+					normal.X*secondRadiusWS.Z-normal.Z*secondRadiusWS.X,
+					normal.Y*secondRadiusWS.X-normal.X*secondRadiusWS.Y,
+				),
+			},
+		},
+		deltaPosition.Length() - r.length
+}
