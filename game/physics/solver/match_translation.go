733399aa2d584a260d1230d3d74903173f01982e
Momchil Atanasov <momchil.dev@gmail.com>
2021-06-01 23:42:07 +0300

    wip: simplify match translation constraint

--- game/physics/solver/match_translation.go
+++ game/physics/solver/match_translation.go
@@ -7,48 +7,90 @@ import (
 
 var _ physics.DBConstraintSolver = (*MatchTranslation)(nil)
 
+// NewMatchTranslation creates a new MatchTranslation constraint solver.
+func NewMatchTranslation() *MatchTranslation {
+	result := &MatchTranslation{}
+	result.DBJacobianConstraintSolver = physics.NewDBJacobianConstraintSolver(result.calculate)
+	return result
+}
+
+// MatchTranslation represents the solution for a constraint
+// that keeps a secondary body attached to an offset of the primary body.
+// It is possible to disable the constraint for certain axis of the
+// primary body.
 type MatchTranslation struct {
-	physics.NilDBConstraintSolver
-	PrimaryAnchor sprec.Vec3
-	IgnoreX       bool
-	IgnoreY       bool
-	IgnoreZ       bool
+	*physics.DBJacobianConstraintSolver
+
+	primaryAnchor sprec.Vec3
+	ignoreX       bool
+	ignoreY       bool
+	ignoreZ       bool
 }
 
-func (t *MatchTranslation) CalculateImpulses(ctx physics.DBSolverContext) physics.DBImpulseSolution {
-	primary := ctx.Primary
-	secondary := ctx.Secondary
-	jacobian, drift := t.calculate(primary, secondary)
-	if sprec.Abs(drift) < epsilon {
-		return physics.DBImpulseSolution{}
-	}
-	lambda := jacobian.ImpulseLambda(primary, secondary)
-	return jacobian.ImpulseSolution(primary, secondary, lambda)
+// PrimaryAnchor returns the attachment point on the primary
+// body to which the secondary will match.
+func (t *MatchTranslation) PrimaryAnchor() sprec.Vec3 {
+	return t.primaryAnchor
 }
 
-func (t *MatchTranslation) CalculateNudges(ctx physics.DBSolverContext) physics.DBNudgeSolution {
-	primary := ctx.Primary
-	secondary := ctx.Secondary
-	jacobian, drift := t.calculate(primary, secondary)
-	if sprec.Abs(drift) < epsilon {
-		return physics.DBNudgeSolution{}
-	}
-	lambda := jacobian.NudgeLambda(primary, secondary, drift)
-	return jacobian.NudgeSolution(primary, secondary, lambda)
+// SetPrimaryAnchor changes the attachment point on the primary
+// body.
+func (t *MatchTranslation) SetPrimaryAnchor(anchor sprec.Vec3) *MatchTranslation {
+	t.primaryAnchor = anchor
+	return t
 }
 
-func (t *MatchTranslation) calculate(primary, secondary *physics.Body) (physics.PairJacobian, float32) {
-	firstRadiusWS := sprec.QuatVec3Rotation(primary.Orientation(), t.PrimaryAnchor)
-	firstAnchorWS := sprec.Vec3Sum(primary.Position(), firstRadiusWS)
-	deltaPosition := sprec.Vec3Diff(secondary.Position(), firstAnchorWS)
-	if t.IgnoreX {
-		deltaPosition = sprec.Vec3Diff(deltaPosition, sprec.Vec3Prod(primary.Orientation().OrientationX(), sprec.Vec3Dot(deltaPosition, primary.Orientation().OrientationX())))
+// IgnoreX returns whether the X dimension, relative to the
+// primary body, will be matched.
+func (t *MatchTranslation) IgnoreX() bool {
+	return t.ignoreX
+}
+
+// SetIgnoreX changes whether the X dimension, relative to the
+// primary body, will be considered.
+func (t *MatchTranslation) SetIgnoreX(ignore bool) *MatchTranslation {
+	t.ignoreX = ignore
+	return t
+}
+
+// IgnoreY returns whether the Y dimension, relative to the
+// primary body, will be matched.
+func (t *MatchTranslation) IgnoreY() bool {
+	return t.ignoreY
+}
+
+// SetIgnoreY changes whether the Y dimension, relative to the
+// primary body, will be considered.
+func (t *MatchTranslation) SetIgnoreY(ignore bool) *MatchTranslation {
+	t.ignoreY = ignore
+	return t
+}
+
+// IgnoreZ returns whether the Z dimension, relative to the
+// primary body, will be matched.
+func (t *MatchTranslation) IgnoreZ() bool {
+	return t.ignoreZ
+}
+
+// SetIgnoreZ changes whether the Z dimension, relative to the
+// primary body, will be considered.
+func (t *MatchTranslation) SetIgnoreZ(ignore bool) *MatchTranslation {
+	t.ignoreZ = ignore
+	return t
+}
+
+func (t *MatchTranslation) calculate(ctx physics.DBSolverContext) (physics.PairJacobian, float32) {
+	firstRadiusWS := sprec.QuatVec3Rotation(ctx.Primary.Orientation(), t.primaryAnchor)
+	firstAnchorWS := sprec.Vec3Sum(ctx.Primary.Position(), firstRadiusWS)
+	deltaPosition := sprec.Vec3Diff(ctx.Secondary.Position(), firstAnchorWS)
+	if t.ignoreX {
+		deltaPosition = sprec.Vec3Diff(deltaPosition, sprec.Vec3Prod(ctx.Primary.Orientation().OrientationX(), sprec.Vec3Dot(deltaPosition, ctx.Primary.Orientation().OrientationX())))
 	}
-	if t.IgnoreY {
-		deltaPosition = sprec.Vec3Diff(deltaPosition, sprec.Vec3Prod(primary.Orientation().OrientationY(), sprec.Vec3Dot(deltaPosition, primary.Orientation().OrientationY())))
+	if t.ignoreY {
+		deltaPosition = sprec.Vec3Diff(deltaPosition, sprec.Vec3Prod(ctx.Primary.Orientation().OrientationY(), sprec.Vec3Dot(deltaPosition, ctx.Primary.Orientation().OrientationY())))
 	}
-	if t.IgnoreZ {
-		deltaPosition = sprec.Vec3Diff(deltaPosition, sprec.Vec3Prod(primary.Orientation().OrientationZ(), sprec.Vec3Dot(deltaPosition, primary.Orientation().OrientationZ())))
+	if t.ignoreZ {
+		deltaPosition = sprec.Vec3Diff(deltaPosition, sprec.Vec3Prod(ctx.Primary.Orientation().OrientationZ(), sprec.Vec3Dot(deltaPosition, ctx.Primary.Orientation().OrientationZ())))
 	}
 	normal := sprec.BasisXVec3()
 	if deltaPosition.SqrLength() > sqrEpsilon {
