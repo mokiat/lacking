62376dce1232c96252023a211436c773b1b86cad
Momchil Atanasov <momchil.dev@gmail.com>
2021-07-17 18:47:33 +0300

    Add switch component

---
+++ ui/mat/switch.go
@@ -0,0 +1,22 @@
+package mat
+
+import t "github.com/mokiat/lacking/ui/template"
+
+type SwitchData struct {
+	VisibleChildIndex int
+}
+
+var Switch = t.ShallowCached(t.Plain(func(props t.Properties) t.Instance {
+	var data SwitchData
+	props.InjectData(&data)
+
+	return t.New(Container, func() {
+		t.WithData(ContainerData{
+			Layout: NewFillLayout(),
+		})
+		t.WithLayoutData(props.LayoutData())
+		if (0 <= data.VisibleChildIndex) && (data.VisibleChildIndex < len(props.Children())) {
+			t.WithChild("visible-child", props.Children()[data.VisibleChildIndex])
+		}
+	})
+}))
