1047c64621e02ca41cc6790b2b925c7783d2b1c3
Momchil Atanasov <momchil.dev@gmail.com>
2021-07-17 18:46:59 +0300

    Rework container and view components

--- ui/mat/container.go
+++ ui/mat/container.go
@@ -2,23 +2,36 @@ package mat
 
 import (
 	"github.com/mokiat/lacking/ui"
+	"github.com/mokiat/lacking/ui/optional"
 	t "github.com/mokiat/lacking/ui/template"
 )
 
 type ContainerData struct {
-	BackgroundColor ui.Color
+	BackgroundColor optional.Color
 	Layout          Layout
 }
 
 var Container = t.ShallowCached(t.Plain(func(props t.Properties) t.Instance {
-	var data ContainerData
+	var (
+		data    ContainerData
+		essence *containerEssence
+	)
 	props.InjectData(&data)
+
+	t.UseState(func() interface{} {
+		return &containerEssence{}
+	}).Inject(&essence)
+
+	if data.BackgroundColor.Specified {
+		essence.backgroundColor = data.BackgroundColor.Value
+	} else {
+		essence.backgroundColor = ui.Transparent()
+	}
+	essence.layout = data.Layout
 
 	return t.New(Element, func() {
 		t.WithData(ElementData{
-			Essence: &containerEssence{
-				ContainerData: data,
-			},
+			Essence: essence,
 		})
 		t.WithLayoutData(props.LayoutData())
 		t.WithChildren(props.Children())
@@ -29,16 +42,17 @@ var _ ui.ElementResizeHandler = (*containerEssence)(nil)
 var _ ui.ElementRenderHandler = (*containerEssence)(nil)
 
 type containerEssence struct {
-	ContainerData
+	backgroundColor ui.Color
+	layout          Layout
 }
 
 func (l *containerEssence) OnResize(element *ui.Element, bounds ui.Bounds) {
-	l.Layout.Apply(element)
+	l.layout.Apply(element)
 }
 
 func (l *containerEssence) OnRender(element *ui.Element, canvas ui.Canvas) {
-	if !l.BackgroundColor.Transparent() {
-		canvas.SetSolidColor(l.BackgroundColor)
+	if !l.backgroundColor.Transparent() {
+		canvas.SetSolidColor(l.backgroundColor)
 		canvas.FillRectangle(
 			ui.NewPosition(0, 0),
 			element.Bounds().Size,
--- ui/mat/view.go
+++ ui/mat/view.go
@@ -1,7 +1,6 @@
 package mat
 
 import (
-	"github.com/mokiat/lacking/ui"
 	"github.com/mokiat/lacking/ui/optional"
 	t "github.com/mokiat/lacking/ui/template"
 )
@@ -12,56 +11,19 @@ type ViewData struct {
 }
 
 var View = t.ShallowCached(t.Plain(func(props t.Properties) t.Instance {
-	var (
-		data    ViewData
-		essence *viewEssence
-	)
+	var data ViewData
 	props.InjectData(&data)
 
-	t.UseState(func() interface{} {
-		return &viewEssence{
-			backgroundColor: ui.Transparent(),
-		}
-	}).Inject(&essence)
-
-	if data.BackgroundColor.Specified {
-		essence.backgroundColor = data.BackgroundColor.Value
-	} else {
-		essence.backgroundColor = ui.Transparent()
-	}
 	if data.FontSrc != "" {
 		t.OpenFontCollection(data.FontSrc)
 	}
 
-	return t.New(Element, func() {
-		t.WithData(ElementData{
-			Essence: essence,
+	return t.New(Container, func() {
+		t.WithData(ContainerData{
+			BackgroundColor: data.BackgroundColor,
+			Layout:          NewFillLayout(),
 		})
 		t.WithLayoutData(props.LayoutData())
 		t.WithChildren(props.Children())
 	})
 }))
-
-var _ ui.ElementResizeHandler = (*viewEssence)(nil)
-var _ ui.ElementRenderHandler = (*viewEssence)(nil)
-
-type viewEssence struct {
-	backgroundColor ui.Color
-}
-
-func (v *viewEssence) OnResize(element *ui.Element, bounds ui.Bounds) {
-	contentBounds := element.ContentBounds()
-	for childElement := element.FirstChild(); childElement != nil; childElement = childElement.RightSibling() {
-		childElement.SetBounds(contentBounds)
-	}
-}
-
-func (v *viewEssence) OnRender(element *ui.Element, canvas ui.Canvas) {
-	if !v.backgroundColor.Transparent() {
-		canvas.SetSolidColor(v.backgroundColor)
-		canvas.FillRectangle(
-			ui.NewPosition(0, 0),
-			element.Bounds().Size,
-		)
-	}
-}
