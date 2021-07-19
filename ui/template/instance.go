3409ba7c4fef84a658c5d712a2a2220bdbdc167b
Momchil Atanasov <momchil.dev@gmail.com>
2021-07-19 22:46:36 +0300

    Add context concept to template package

--- ui/template/instance.go
+++ ui/template/instance.go
@@ -33,3 +33,16 @@ func (i Instance) hasMatchingChild(instance Instance) bool {
 	}
 	return false
 }
+
+// WithContext can be used during the instantiation of an Application
+// in order to configure a context object.
+//
+// This is a helper function in place of RegisterContext. While currently not
+// enforced, you should use this function during the instantiation of your
+// root component.
+// Using it at a later point during the lifecycle of your application could
+// indicate an improper usage of contexts. You may consider using reducers
+// and global state instead.
+func WithContext(context interface{}) {
+	RegisterContext(context)
+}
