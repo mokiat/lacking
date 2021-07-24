e96e2b4908f913be9deb574cf54dece11ed8a488
Momchil Atanasov <momchil.dev@gmail.com>
2021-07-24 15:45:12 +0300

    Simplify AnchorLayout configuration

--- ui/mat/common.go
+++ ui/mat/common.go
@@ -12,17 +12,6 @@ const (
 	AlignmentBottom
 )
 
-// Relation determines relative to what is a position calculated.
-type Relation int
-
-const (
-	RelationLeft Relation = 1 + iota
-	RelationRight
-	RelationTop
-	RelationBottom
-	RelationCenter
-)
-
 // ClickListener can be used to get notifications about
 // click events.
 type ClickListener func()
