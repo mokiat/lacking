daced3321ba9c5c1d490ad9597c259b6f45c15f4
Momchil Atanasov <momchil.dev@gmail.com>
2021-10-17 22:23:22 +0300

    Rework file resource locator

--- ui/resource.go
+++ ui/resource.go
@@ -2,7 +2,7 @@ package ui
 
 import (
 	"io"
-	"os"
+	"io/fs"
 )
 
 // ResourceLocator represents a logic by which resources
@@ -18,10 +18,22 @@ type ResourceLocator interface {
 	OpenResource(uri string) (io.ReadCloser, error)
 }
 
+// NewFileResourceLocator returns a new FileResourceLocator that is
+// configured to search for resources relative to dir.
+func NewFileResourceLocator(dir fs.FS) *FileResourceLocator {
+	return &FileResourceLocator{
+		dir: dir,
+	}
+}
+
 // FileResourceLocator is an implementation of ResourceLocator that
 // uses the local filesystem to open resources.
-type FileResourceLocator struct{}
+type FileResourceLocator struct {
+	dir fs.FS
+}
 
-func (l FileResourceLocator) OpenResource(uri string) (io.ReadCloser, error) {
-	return os.Open(uri)
+// OpenResource opens the resource at the specified
+// relative address path.
+func (l *FileResourceLocator) OpenResource(uri string) (io.ReadCloser, error) {
+	return l.dir.Open(uri)
 }
