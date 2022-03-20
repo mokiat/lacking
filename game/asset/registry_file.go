0b0b941954e41f289db042d4a090cf440fd70071
Momchil Atanasov <momchil.dev@gmail.com>
2022-03-20 18:22:13 +0200

    Add http-based asset registry implementation

--- game/asset/registry_file.go
+++ game/asset/registry_file.go
@@ -229,22 +229,3 @@ func (r *DirRegistry) previewFile(guid string) string {
 func (r *DirRegistry) contentFile(guid string) string {
 	return filepath.Join(r.contentDir, fmt.Sprintf("%s.dat", guid))
 }
-
-type resourcesDTO struct {
-	Resources []resourceDTO `yaml:"resources"`
-}
-
-type resourceDTO struct {
-	GUID string `yaml:"guid"`
-	Kind string `yaml:"kind"`
-	Name string `yaml:"name"`
-}
-
-type dependenciesDTO struct {
-	Dependencies []dependencyDTO `yaml:"dependencies"`
-}
-
-type dependencyDTO struct {
-	SourceGUID string `yaml:"source_guid"`
-	TargetGUID string `yaml:"target_guid"`
-}
