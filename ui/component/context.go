9933c896b3ad0f943913a547345c80d383af126f
Momchil Atanasov <momchil.dev@gmail.com>
2022-04-28 22:57:40 +0300

    Add generic GetContext function

--- ui/component/context.go
+++ ui/component/context.go
@@ -5,26 +5,26 @@ import (
 	"reflect"
 )
 
-var contexts map[reflect.Type]interface{}
+var contexts map[reflect.Type]any
 
 func init() {
-	contexts = make(map[reflect.Type]interface{})
+	contexts = make(map[reflect.Type]any)
 }
 
 // RegisterContext registers a data structure that will be
 // accessible from all components.
 //
-// If used, this method should be use during bootstrapping
+// If used, this method should be called during bootstrapping
 // and should not be called from within components.
 //
 // The context is stored according to its type and there can
-// be only one call per struct type. Once a context is set
+// only be one call per struct type. Once a context is set
 // it is persisted for the whole lifecycle of the framework.
 //
 // Contexts should be used only for global configurations
-// that will not change, like graphics handles or translation
+// that will not change, like graphics handles or i18n
 // functions.
-func RegisterContext(value interface{}) {
+func RegisterContext(value any) {
 	valueType := reflect.TypeOf(value)
 	if _, ok := contexts[valueType]; ok {
 		panic(fmt.Errorf("a context of the specified type %T has already been registered", value))
@@ -32,12 +32,23 @@ func RegisterContext(value interface{}) {
 	contexts[valueType] = value
 }
 
+// GetContext retrieves the appropriate context based on the generic type param
+// and returns it.
+func GetContext[T any]() T {
+	var result T
+	contextValue, ok := contexts[reflect.TypeOf(result)]
+	if !ok {
+		panic(fmt.Errorf("there is no context of type %T", result))
+	}
+	return contextValue.(T)
+}
+
 // InjectContext retrieves the appropriate context and
 // assigns it to target.
 //
 // The specified target must be a pointer to the type
 // that was used in RegisterContext.
-func InjectContext(target interface{}) {
+func InjectContext(target any) {
 	if target == nil {
 		panic("target cannot be nil")
 	}
