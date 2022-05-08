367df728ef04f34f51c32443010f7f60cc95f364
Momchil Atanasov <momchil.dev@gmail.com>
2022-05-08 16:02:12 +0300

    Configure color theme

--- ui/mat/theme.go
+++ ui/mat/theme.go
@@ -3,29 +3,33 @@ package mat
 import "github.com/mokiat/lacking/ui"
 
 var (
-	PrimaryColor        ui.Color = ui.RGB(0x21, 0x96, 0xF3)
-	OnPrimaryColor      ui.Color = ui.RGB(0x00, 0x00, 0x00)
-	PrimaryLightColor   ui.Color = ui.RGB(0x00, 0x00, 0x00) // TODO
-	OnPrimaryLightColor ui.Color = ui.RGB(0x00, 0x00, 0x00) // TODO
-	PrimaryDarkColor    ui.Color = ui.RGB(0x19, 0x76, 0xD2)
-	OnPrimaryDarkColor  ui.Color = ui.RGB(0x19, 0x76, 0xD2) // TODO
+	PrimaryColor   ui.Color = ui.RGB(0x40, 0x89, 0xBD)
+	OnPrimaryColor ui.Color = ui.RGB(0x00, 0x00, 0x00)
 
-	SecondaryColor        ui.Color = ui.RGB(0x8B, 0xC3, 0x4A)
-	OnSecondaryColor      ui.Color = ui.RGB(0x00, 0x00, 0x00)
-	SecondaryLightColor   ui.Color = ui.RGB(0x00, 0x00, 0x00) // TODO
-	OnSecondaryLightColor ui.Color = ui.RGB(0x00, 0x00, 0x00) // TODO
-	SecondaryDarkColor    ui.Color = ui.RGB(0x68, 0x9F, 0x38)
-	OnSecondaryDarkColor  ui.Color = ui.RGB(0x68, 0x9F, 0x38) // TODO
+	PrimaryLightColor   ui.Color = ui.RGB(0x7C, 0xAE, 0xEB)
+	OnPrimaryLightColor ui.Color = ui.RGB(0x00, 0x00, 0x00)
 
-	BackgroundColor   ui.Color = ui.RGB(0xFF, 0xFF, 0xFF)
-	OnBackgroundColor ui.Color = ui.RGB(0xFF, 0xFF, 0xFF) // TODO
+	PrimaryDarkColor   ui.Color = ui.RGB(0x32, 0x59, 0x73)
+	OnPrimaryDarkColor ui.Color = ui.RGB(0xFF, 0xFF, 0xFF)
+
+	SecondaryColor   ui.Color = ui.RGB(0x8B, 0xC3, 0x4A)
+	OnSecondaryColor ui.Color = ui.RGB(0x00, 0x00, 0x00)
+
+	SecondaryLightColor   ui.Color = ui.RGB(0xA9, 0xC2, 0x8A)
+	OnSecondaryLightColor ui.Color = ui.RGB(0x00, 0x00, 0x00)
+
+	SecondaryDarkColor   ui.Color = ui.RGB(0x4B, 0x6B, 0x3A)
+	OnSecondaryDarkColor ui.Color = ui.RGB(0xFF, 0xFF, 0xFF)
+
+	BackgroundColor   ui.Color = ui.RGB(0x3B, 0x3B, 0x3B)
+	OnBackgroundColor ui.Color = ui.RGB(0xFF, 0xFF, 0xFF)
 
 	SurfaceColor        ui.Color = ui.RGB(0xFF, 0xFF, 0xFF)
 	OnSurfaceColor      ui.Color = ui.RGB(0x00, 0x00, 0x00)
 	OnSurfaceHoverColor ui.Color = ui.RGB(0xDD, 0xDD, 0xDD)
 	OnSurfacePressColor ui.Color = ui.RGB(0x66, 0x66, 0x66)
 
-	ErrorColor   ui.Color = ui.RGB(0xB0, 0x00, 0x20)
+	ErrorColor   ui.Color = ui.RGB(0xB3, 0x1E, 0x00)
 	OnErrorColor ui.Color = ui.RGB(0xFF, 0xFF, 0xFF)
 
 	OutlineColor ui.Color = ui.RGB(0x80, 0x80, 0x80)
