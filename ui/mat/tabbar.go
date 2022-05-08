5cc58513e7c8845ee084a7e052359df661804acc
Momchil Atanasov <momchil.dev@gmail.com>
2022-05-08 16:02:54 +0300

    Add tabbar component

---
+++ ui/mat/tabbar.go
@@ -0,0 +1,40 @@
+package mat
+
+import (
+	"github.com/mokiat/lacking/ui"
+	co "github.com/mokiat/lacking/ui/component"
+	"github.com/mokiat/lacking/util/optional"
+)
+
+var (
+	TabbarHeight      = 40
+	TabbarItemSpacing = 2
+	TabbarSidePadding = 5
+)
+
+// Tabbar is a container intended to hold Tab components.
+var Tabbar = co.ShallowCached(co.Define(func(props co.Properties) co.Instance {
+	var (
+		layoutData = co.GetOptionalLayoutData(props, LayoutData{})
+	)
+
+	// force specific height
+	layoutData.Height = optional.Value(TabbarHeight)
+
+	return co.New(Container, func() {
+		co.WithData(ContainerData{
+			BackgroundColor: optional.Value(PrimaryLightColor),
+			Padding: ui.Spacing{
+				Left:  TabbarSidePadding,
+				Right: TabbarSidePadding,
+			},
+			Layout: NewHorizontalLayout(HorizontalLayoutSettings{
+				ContentAlignment: AlignmentCenter,
+				ContentSpacing:   TabbarItemSpacing,
+			}),
+		})
+
+		co.WithLayoutData(layoutData)
+		co.WithChildren(props.Children())
+	})
+}))
