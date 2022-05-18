4111dcb3079c0389783f11c0084c14b2bed89bfe
Momchil Atanasov <momchil.dev@gmail.com>
2022-05-18 23:12:33 +0300

    Change reference type to int32

--- render/pipeline.go
+++ render/pipeline.go
@@ -70,7 +70,7 @@ type StencilOperationState struct {
 	PassOp         StencilOperation
 	Comparison     Comparison
 	ComparisonMask uint32
-	Reference      uint32
+	Reference      int32
 	WriteMask      uint32
 }
 
