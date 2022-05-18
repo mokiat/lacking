4d50b664c1909cdd6c1300d07bfe5cdf9d99be44
Momchil Atanasov <momchil.dev@gmail.com>
2022-05-18 22:49:53 +0300

    Add debug flag to log package

--- log/log_debug.go
+++ log/log_debug.go
@@ -4,6 +4,9 @@ package log
 
 import "log"
 
+// DebugEnabled is a flag indicating the state of debug level.
+var DebugEnabled = true
+
 // Debug logs a debug message.
 func Debug(format string, args ...interface{}) {
 	log.Printf("[debug] "+format, args...)
--- log/log_nodebug.go
+++ log/log_nodebug.go
@@ -2,6 +2,9 @@
 
 package log
 
+// DebugEnabled is a flag indicating the state of debug level.
+var DebugEnabled = false
+
 // Debug logs a debug message.
 func Debug(format string, args ...interface{}) {
 }
