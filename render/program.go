145ae1ecaf6a61940fd41ab7abdba6671e8a957a
Momchil Atanasov <momchil.dev@gmail.com>
2022-07-03 17:52:05 +0300

    Allow preconfiguration of texture bindings

--- render/program.go
+++ render/program.go
@@ -1,8 +1,21 @@
 package render
 
 type ProgramInfo struct {
-	VertexShader   Shader
-	FragmentShader Shader
+	VertexShader    Shader
+	FragmentShader  Shader
+	TextureBindings []TextureBinding
+}
+
+func NewTextureBinding(name string, index int) TextureBinding {
+	return TextureBinding{
+		Name:  name,
+		Index: index,
+	}
+}
+
+type TextureBinding struct {
+	Name  string
+	Index int
 }
 
 type UniformLocation interface{}
