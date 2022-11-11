653bafd01352301c7f980b7f07e32413de87a674
Momchil Atanasov <momchil.dev@gmail.com>
2022-11-11 10:42:31 +0200

    Check point transform performance

--- util/shape/point.go
+++ util/shape/point.go
@@ -2,12 +2,17 @@ package shape
 
 import "github.com/mokiat/gomath/dprec"
 
+// Note: Having to cast Point to Vec3 and vice versa does not affect
+// performance in any way.
+
 // Point represents a single point in 3D space.
 type Point dprec.Vec3
 
 // Transformed returns a new Point that is based on this one but with the
 // specified Transform applied to it.
 func (p Point) Transformed(parent Transform) Point {
+	// Note: Doing an identity check on the transform,
+	// as a form of quick return, actually worsens the performance.
 	return Point(dprec.Vec3Sum(
 		parent.position,
 		dprec.QuatVec3Rotation(parent.rotation, dprec.Vec3(p)),
