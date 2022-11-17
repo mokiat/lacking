c78722f6713247e982ea13f1bdf9d616ac248afc
Momchil Atanasov <momchil.dev@gmail.com>
2022-11-17 11:14:39 +0200

    Add functions for moment of inertia of a sphere

--- game/physics/inertia.go
+++ game/physics/inertia.go
@@ -12,3 +12,15 @@ func SymmetricMomentOfInertia(value float64) dprec.Mat3 {
 		0.0, 0.0, value,
 	)
 }
+
+// SolidSphereMomentOfInertia returns the moment of inertia of a solid
+// sphere with the specified mass and radius.
+func SolidSphereMomentOfInertia(mass, radius float64) dprec.Mat3 {
+	return SymmetricMomentOfInertia(mass * radius * radius * (2.0 / 5.0))
+}
+
+// HollowSphereMomentOfInertia returns the moment of inertia of a hollow
+// sphere with the specified mass and radius.
+func HollowSphereMomentOfInertia(mass, radius float64) dprec.Mat3 {
+	return SymmetricMomentOfInertia(mass * radius * radius * (2.0 / 3.0))
+}
