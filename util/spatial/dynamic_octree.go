2e66bb288689a36460c2400028d573d998054c46
Momchil Atanasov <momchil.dev@gmail.com>
2023-07-01 18:40:42 +0300

    Add dynamic octree implementation

--- util/spatial/dynamic_octree.go
+++ util/spatial/dynamic_octree.go
@@ -1,25 +1,28 @@
 package spatial
 
 import (
+	"github.com/mokiat/gog/ds"
 	"github.com/mokiat/gog/opt"
 	"github.com/mokiat/gomath/dprec"
 	"golang.org/x/exp/slices"
 )
 
-var staticOctreeLogger = spatialLogger.Path("/static-octree")
+var dynamicOctreeLogger = spatialLogger.Path("/dynamic-octree")
 
-const unspecifiedIndex = int32(-1)
+// DynamicOctreeItemID is an identifier used to control the placement of an item
+// into a dynamic octree.
+type DynamicOctreeItemID uint32
 
-// StaticOctreeStats represents the current state of a StaticOctree.
-type StaticOctreeStats struct {
+// DynamicOctreeStats represents the current state of a DynamicOctree.
+type DynamicOctreeStats struct {
 	NodeCount          int32
 	ItemCount          int32
 	ItemsCountPerDepth []int32
 }
 
-// StaticOctreeVisitStats represents statistics on the last visit operation
-// performed on a StaticOctree.
-type StaticOctreeVisitStats struct {
+// DynamicOctreeVisitStats represents statistics on the last visit operation
+// performed on a DynamicOctree.
+type DynamicOctreeVisitStats struct {
 	NodeCount         int32
 	NodeCountVisited  int32
 	NodeCountAccepted int32
@@ -30,8 +33,8 @@ type StaticOctreeVisitStats struct {
 	ItemCountRejected int32
 }
 
-// StaticOctreeSettings contains the settings for a StaticOctree.
-type StaticOctreeSettings struct {
+// DynamicOctreeSettings contains the settings for a DynamicOctree.
+type DynamicOctreeSettings struct {
 
 	// Size specifies the dimension of the octree cube. If not specified,
 	// then a default size of 4096 will be used. Inserting an item outside
@@ -65,8 +68,8 @@ type StaticOctreeSettings struct {
 	InitialItemCapacity opt.T[int32]
 }
 
-// NewStaticOctree creates a new StaticOctree using the provided settings.
-func NewStaticOctree[T any](settings StaticOctreeSettings) *StaticOctree[T] {
+// NewDynamicOctree creates a new DynamicOctree using the provided settings.
+func NewDynamicOctree[T any](settings DynamicOctreeSettings) *DynamicOctree[T] {
 	size := 4096.0
 	if settings.Size.Specified {
 		size = settings.Size.Value
@@ -103,35 +106,42 @@ func NewStaticOctree[T any](settings StaticOctreeSettings) *StaticOctree[T] {
 		}
 	}
 
-	nodes := make([]staticOctreeNode[T], 0, initialNodeCapacity)
-	nodes = append(nodes, staticOctreeNode[T]{
-		itemStart: unspecifiedIndex,
-		itemEnd:   unspecifiedIndex,
+	nodes := make([]dynamicOctreeNode[T], 0, initialNodeCapacity)
+	nodes = append(nodes, dynamicOctreeNode[T]{
+		parent: unspecifiedIndex,
 		children: [8]int32{
 			unspecifiedIndex, unspecifiedIndex, unspecifiedIndex, unspecifiedIndex,
 			unspecifiedIndex, unspecifiedIndex, unspecifiedIndex, unspecifiedIndex,
 		},
-		x: 0.0,
-		y: 0.0,
-		z: 0.0,
-		s: size,
+		itemStart: unspecifiedIndex,
+		itemEnd:   unspecifiedIndex,
+		x:         0.0,
+		y:         0.0,
+		z:         0.0,
+		s:         size,
 	})
-	items := make([]staticOctreeItem[T], 0, initialItemCapacity)
+	items := make([]dynamicOctreeItem[T], 0, initialItemCapacity)
+	idMappings := make([]int32, 0, initialItemCapacity)
 
-	return &StaticOctree[T]{
-		nodes:     nodes,
-		items:     items,
-		biasRatio: biasRatio,
-		maxDepth:  maxDepth,
+	return &DynamicOctree[T]{
+		nodes:           nodes,
+		items:           items,
+		freeNodeIndices: ds.NewStack[int32](32),
+		freeItemIndices: ds.NewStack[int32](32),
+		idMappings:      idMappings,
+		biasRatio:       biasRatio,
+		maxDepth:        maxDepth,
 	}
 }
 
-// StaticOctree represents an octree spatial structure that only allows the
-// insertion of static items. Such items cannot be resized, repositioned, or
-// removed from the tree.
-type StaticOctree[T any] struct {
-	nodes []staticOctreeNode[T]
-	items []staticOctreeItem[T]
+// DynamicOctree is a spatial structure that uses a loose octree implementation
+// with biased placement to enable the fast search of items within a region.
+type DynamicOctree[T any] struct {
+	nodes           []dynamicOctreeNode[T]
+	items           []dynamicOctreeItem[T]
+	freeNodeIndices *ds.Stack[int32]
+	freeItemIndices *ds.Stack[int32]
+	idMappings      []int32
 
 	biasRatio float64
 	maxDepth  int32
@@ -145,12 +155,12 @@ type StaticOctree[T any] struct {
 }
 
 // Stats returns statistics on the current state of this octree.
-func (t *StaticOctree[T]) Stats() StaticOctreeStats {
+func (t *DynamicOctree[T]) Stats() DynamicOctreeStats {
 	itemCountPerDepth := make([]int32, t.maxDepth+1)
 	for i := int32(1); i <= t.maxDepth; i++ {
 		itemCountPerDepth[i] = t.itemsAtDepth(0, 1, i)
 	}
-	return StaticOctreeStats{
+	return DynamicOctreeStats{
 		NodeCount:          int32(len(t.nodes)),
 		ItemCount:          int32(len(t.items)),
 		ItemsCountPerDepth: itemCountPerDepth,
@@ -159,13 +169,13 @@ func (t *StaticOctree[T]) Stats() StaticOctreeStats {
 
 // VisitStats returns statistics information on the last executed search in
 // this octree.
-func (t *StaticOctree[T]) VisitStats() StaticOctreeVisitStats {
-	return StaticOctreeVisitStats{
-		NodeCount:         int32(len(t.nodes)),
+func (t *DynamicOctree[T]) VisitStats() DynamicOctreeVisitStats {
+	return DynamicOctreeVisitStats{
+		NodeCount:         int32(len(t.nodes) - t.freeNodeIndices.Size()),
 		NodeCountVisited:  t.nodeCountAccepted + t.nodeCountRejected,
 		NodeCountAccepted: t.nodeCountAccepted,
 		NodeCountRejected: t.nodeCountRejected,
-		ItemCount:         int32(len(t.items)),
+		ItemCount:         int32(len(t.items) - t.freeItemIndices.Size()),
 		ItemCountVisited:  t.itemCountAccepted + t.itemCountRejected,
 		ItemCountAccepted: t.itemCountAccepted,
 		ItemCountRejected: t.itemCountRejected,
@@ -174,35 +184,67 @@ func (t *StaticOctree[T]) VisitStats() StaticOctreeVisitStats {
 
 // Insert adds an item to this octree at the specified position and taking the
 // specified radius into account.
-func (t *StaticOctree[T]) Insert(position dprec.Vec3, radius float64, item T) {
-	if len(t.items) == cap(t.items) {
-		staticOctreeLogger.Warn("Item slice capacity %d reached. Will grow.", len(t.items))
-	}
+func (t *DynamicOctree[T]) Insert(position dprec.Vec3, radius float64, value T) DynamicOctreeItemID {
 	t.isDirty = true
-	nodeIndex := t.pickNodeForItem(position, radius)
-	t.items = append(t.items, staticOctreeItem[T]{
-		node:     nodeIndex,
-		position: position,
-		radius:   radius,
-		item:     item,
-	})
+	if !t.freeItemIndices.IsEmpty() {
+		itemIndex := t.freeItemIndices.Pop()
+		item := &t.items[itemIndex]
+		item.position = position
+		item.radius = radius
+		item.value = value
+		item.node = t.pickNodeForItem(position, radius)
+		return item.id
+	} else {
+		if len(t.items) == cap(t.items) {
+			dynamicOctreeLogger.Warn("Item slice capacity %d reached. Will grow.", len(t.items))
+		}
+		id := DynamicOctreeItemID(len(t.items))
+		t.idMappings = append(t.idMappings, int32(id))
+		nodeIndex := t.pickNodeForItem(position, radius)
+		t.items = append(t.items, dynamicOctreeItem[T]{
+			id:       id,
+			node:     nodeIndex,
+			position: position,
+			radius:   radius,
+			value:    value,
+		})
+		return id
+	}
+}
+
+// Update repositions the item with the specified id to the new position and
+// radius.
+func (t *DynamicOctree[T]) Update(id DynamicOctreeItemID, position dprec.Vec3, radius float64) {
+	t.isDirty = true
+	itemIndex := t.idMappings[id]
+	item := &t.items[itemIndex]
+	item.position = position
+	item.radius = radius
+	item.node = t.pickNodeForItem(position, radius)
+}
+
+// Remove removes the item with the specified id from this data structure.
+func (t *DynamicOctree[T]) Remove(id DynamicOctreeItemID) {
+	t.isDirty = true
+	itemIndex := t.idMappings[id]
+	item := &t.items[itemIndex]
+	item.node = unspecifiedIndex
+	t.freeItemIndices.Push(itemIndex)
 }
 
 // VisitHexahedronRegion finds all items that are inside or intersect the
 // specified hexahedron region. It calls the specified visitor for each item
 // found.
-func (t *StaticOctree[T]) VisitHexahedronRegion(region *HexahedronRegion, visitor Visitor[T]) {
+func (t *DynamicOctree[T]) VisitHexahedronRegion(region *HexahedronRegion, visitor Visitor[T]) {
 	t.nodeCountAccepted = 0
 	t.nodeCountRejected = 0
 	t.itemCountAccepted = 0
 	t.itemCountRejected = 0
-	if t.isDirty {
-		t.refresh()
-	}
+	t.refresh()
 	t.visitNodeInHexahedronRegion(0, region, visitor)
 }
 
-func (t *StaticOctree[T]) pickNodeForItem(position dprec.Vec3, radius float64) int32 {
+func (t *DynamicOctree[T]) pickNodeForItem(position dprec.Vec3, radius float64) int32 {
 	bestNodeIndex := unspecifiedIndex
 	currentNodeIndex := int32(0)
 	depth := int32(0)
@@ -217,7 +259,7 @@ func (t *StaticOctree[T]) pickNodeForItem(position dprec.Vec3, radius float64) i
 	return bestNodeIndex
 }
 
-func (t *StaticOctree[T]) pickChildNode(parentNodeIndex int32, position dprec.Vec3, radius float64) int32 {
+func (t *DynamicOctree[T]) pickChildNode(parentNodeIndex int32, position dprec.Vec3, radius float64) int32 {
 	parentNode := &t.nodes[parentNodeIndex]
 	childSize := parentNode.s / 2.0
 	if radius*t.biasRatio > childSize {
@@ -254,30 +296,43 @@ func (t *StaticOctree[T]) pickChildNode(parentNodeIndex int32, position dprec.Ve
 	if parentNode.children[childIndex] != unspecifiedIndex {
 		return parentNode.children[childIndex]
 	}
-	childNodeIndex := int32(len(t.nodes)) // predict next node index
-	parentNode.children[childIndex] = childNodeIndex
 
-	if len(t.nodes) == cap(t.nodes) {
-		staticOctreeLogger.Warn("Node slice capacity %d reached. Will grow.", len(t.nodes))
+	if !t.freeNodeIndices.IsEmpty() {
+		childNodeIndex := t.freeNodeIndices.Pop()
+		parentNode.children[childIndex] = childNodeIndex
+		childNode := &t.nodes[childNodeIndex]
+		childNode.parent = parentNodeIndex
+		childNode.x = childX
+		childNode.y = childY
+		childNode.z = childZ
+		childNode.s = childSize
+		return childNodeIndex
+	} else {
+		if len(t.nodes) == cap(t.nodes) {
+			staticOctreeLogger.Warn("Node slice capacity %d reached. Will grow.", len(t.nodes))
+		}
+		childNodeIndex := int32(len(t.nodes)) // predict next node index
+		parentNode.children[childIndex] = childNodeIndex
+		// NOTE: DO NOT use parentNode after this append as the ref might be towards
+		// an old slice.
+		t.nodes = append(t.nodes, dynamicOctreeNode[T]{
+			parent:    parentNodeIndex,
+			itemStart: unspecifiedIndex,
+			itemEnd:   unspecifiedIndex,
+			children: [8]int32{
+				unspecifiedIndex, unspecifiedIndex, unspecifiedIndex, unspecifiedIndex,
+				unspecifiedIndex, unspecifiedIndex, unspecifiedIndex, unspecifiedIndex,
+			},
+			x: childX,
+			y: childY,
+			z: childZ,
+			s: childSize,
+		})
+		return childNodeIndex
 	}
-	// NOTE: DO NOT use parentNode after this append as the ref might be towards
-	// an old slice.
-	t.nodes = append(t.nodes, staticOctreeNode[T]{
-		itemStart: unspecifiedIndex,
-		itemEnd:   unspecifiedIndex,
-		children: [8]int32{
-			unspecifiedIndex, unspecifiedIndex, unspecifiedIndex, unspecifiedIndex,
-			unspecifiedIndex, unspecifiedIndex, unspecifiedIndex, unspecifiedIndex,
-		},
-		x: childX,
-		y: childY,
-		z: childZ,
-		s: childSize,
-	})
-	return childNodeIndex
 }
 
-func (t *StaticOctree[T]) visitNodeInHexahedronRegion(nodeIndex int32, region *HexahedronRegion, visitor Visitor[T]) {
+func (t *DynamicOctree[T]) visitNodeInHexahedronRegion(nodeIndex int32, region *HexahedronRegion, visitor Visitor[T]) {
 	if nodeIndex == unspecifiedIndex {
 		return
 	}
@@ -287,7 +342,7 @@ func (t *StaticOctree[T]) visitNodeInHexahedronRegion(nodeIndex int32, region *H
 		for itemIndex := node.itemStart; itemIndex < node.itemEnd; itemIndex++ {
 			item := &t.items[itemIndex]
 			if item.isInsideHexahedronRegion(region) {
-				visitor.Visit(item.item)
+				visitor.Visit(item.value)
 				t.itemCountAccepted++
 			} else {
 				t.itemCountRejected++
@@ -301,9 +356,30 @@ func (t *StaticOctree[T]) visitNodeInHexahedronRegion(nodeIndex int32, region *H
 	}
 }
 
-func (t *StaticOctree[T]) refresh() {
-	slices.SortFunc(t.items, compareStaticItems[T])
+func (t *DynamicOctree[T]) refresh() {
+	if t.isDirty {
+		t.sortItems()
+		t.eraseItemOffsets()
+		t.evaluateItemOffsets()
+		t.updateIDMappings()
+		t.gcNodes()
+		t.isDirty = false
+	}
+}
 
+func (t *DynamicOctree[T]) sortItems() {
+	slices.SortFunc(t.items, compareDynamicOctreeItems[T])
+}
+
+func (t *DynamicOctree[T]) eraseItemOffsets() {
+	for i := range t.nodes {
+		node := &t.nodes[i]
+		node.itemStart = unspecifiedIndex
+		node.itemEnd = unspecifiedIndex
+	}
+}
+
+func (t *DynamicOctree[T]) evaluateItemOffsets() {
 	lastNode := unspecifiedIndex
 	itemIndex := int32(0)
 	itemCount := int32(len(t.items))
@@ -321,11 +397,42 @@ func (t *StaticOctree[T]) refresh() {
 	if lastNode != unspecifiedIndex {
 		t.nodes[lastNode].itemEnd = itemIndex
 	}
-
-	t.isDirty = false
 }
 
-func (t *StaticOctree[T]) itemsAtDepth(nodeIndex, currentDepth, depth int32) int32 {
+func (t *DynamicOctree[T]) updateIDMappings() {
+	for i, item := range t.items {
+		t.idMappings[item.id] = int32(i)
+	}
+}
+
+func (t *DynamicOctree[T]) gcNodes() {
+	for i := range t.nodes {
+		t.gcNode(int32(i))
+	}
+}
+
+func (t *DynamicOctree[T]) gcNode(nodeIndex int32) {
+	node := &t.nodes[nodeIndex]
+	if node.parent == unspecifiedIndex {
+		return // already deleted or root
+	}
+	if !node.isEmpty() {
+		return // can't gc node
+	}
+	parentNodeIndex := node.parent
+	parentNode := &t.nodes[parentNodeIndex]
+	for i, childNodeIndex := range parentNode.children {
+		if childNodeIndex == nodeIndex {
+			parentNode.children[i] = unspecifiedIndex
+			break
+		}
+	}
+	node.parent = unspecifiedIndex
+	t.freeNodeIndices.Push(nodeIndex)
+	t.gcNode(parentNodeIndex)
+}
+
+func (t *DynamicOctree[T]) itemsAtDepth(nodeIndex, currentDepth, depth int32) int32 {
 	if nodeIndex == unspecifiedIndex {
 		return 0
 	}
@@ -340,14 +447,15 @@ func (t *StaticOctree[T]) itemsAtDepth(nodeIndex, currentDepth, depth int32) int
 	return result
 }
 
-func compareStaticItems[T any](a, b staticOctreeItem[T]) bool {
+func compareDynamicOctreeItems[T any](a, b dynamicOctreeItem[T]) bool {
 	return a.node < b.node
 }
 
-type staticOctreeNode[T any] struct {
+type dynamicOctreeNode[T any] struct {
+	parent    int32
+	children  [8]int32
 	itemStart int32
 	itemEnd   int32
-	children  [8]int32
 
 	x float64
 	y float64
@@ -355,7 +463,16 @@ type staticOctreeNode[T any] struct {
 	s float64
 }
 
-func (n *staticOctreeNode[T]) isInsideHexahedronRegion(region *HexahedronRegion) bool {
+func (n *dynamicOctreeNode[T]) isEmpty() bool {
+	for _, childIndex := range n.children {
+		if childIndex != unspecifiedIndex {
+			return false
+		}
+	}
+	return n.itemStart >= n.itemEnd
+}
+
+func (n *dynamicOctreeNode[T]) isInsideHexahedronRegion(region *HexahedronRegion) bool {
 	position := dprec.NewVec3(n.x, n.y, n.z)
 	radius := n.s * sizeToDoubleRadius
 	return region[0].ContainsSphere(position, radius) &&
@@ -366,14 +483,15 @@ func (n *staticOctreeNode[T]) isInsideHexahedronRegion(region *HexahedronRegion)
 		region[5].ContainsSphere(position, radius)
 }
 
-type staticOctreeItem[T any] struct {
+type dynamicOctreeItem[T any] struct {
+	id       DynamicOctreeItemID
 	node     int32
 	position dprec.Vec3
 	radius   float64
-	item     T
+	value    T
 }
 
-func (i *staticOctreeItem[T]) isInsideHexahedronRegion(region *HexahedronRegion) bool {
+func (i *dynamicOctreeItem[T]) isInsideHexahedronRegion(region *HexahedronRegion) bool {
 	return region[0].ContainsSphere(i.position, i.radius) &&
 		region[1].ContainsSphere(i.position, i.radius) &&
 		region[2].ContainsSphere(i.position, i.radius) &&
