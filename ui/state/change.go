1e76cbfde16bf6412ea9b4c68ebd328b19d2b0f0
Momchil Atanasov <momchil.dev@gmail.com>
2023-10-28 18:20:13 +0300

    Introduce new state change helpers

--- ui/state/change.go
+++ ui/state/change.go
@@ -1,5 +1,7 @@
 package state
 
+import "time"
+
 // Change represents a state change.
 type Change interface {
 	Apply()
@@ -9,31 +11,75 @@ type Change interface {
 // ExtendableChange represents a change that can be extended
 // (e.g. editbox write).
 type ExtendableChange interface {
+
 	// Extend returns whether the specified change was merged into the current
 	// one.
 	Extend(change Change) bool
 }
 
-// FuncChange is a utility function that creates a change based on the
-// specified apply and revert functions.
-func FuncChange(apply, revert func()) Change {
-	return &funcChange{
-		apply:  apply,
-		revert: revert,
+// Action represents a single unit for work.
+type Action func()
+
+// ActionChange returns a Change that is based on the specified forward and
+// reverse actions.
+func ActionChange(accumDuration time.Duration, forward, reverse []Action) Change {
+	return &actionChange{
+		forward: forward,
+		reverse: reverse,
 	}
 }
 
-type funcChange struct {
-	apply  func()
-	revert func()
+type actionChange struct {
+	forward []Action
+	reverse []Action
 }
 
-func (ch *funcChange) Apply() {
-	ch.apply()
+func (c *actionChange) Apply() {
+	for _, action := range c.forward {
+		action()
+	}
 }
 
-func (ch *funcChange) Revert() {
-	ch.revert()
+func (c *actionChange) Revert() {
+	for _, action := range c.reverse {
+		action()
+	}
+}
+
+// AccumActionChange is similar to ActionChange except that it is also
+// extendable. The accumDuration interval is used to specify accumulation
+// threshold for the change.
+func AccumActionChange(forward, reverse []Action, accumDuration time.Duration) Change {
+	return &accumActionChange{
+		actionChange: actionChange{
+			forward: forward,
+			reverse: reverse,
+		},
+		when:          time.Now(),
+		accumDuration: accumDuration,
+	}
+}
+
+type accumActionChange struct {
+	actionChange
+	when          time.Time
+	accumDuration time.Duration
+}
+
+func (c *accumActionChange) Extend(other Change) bool {
+	otherChange, ok := other.(*accumActionChange)
+	if !ok {
+		return false
+	}
+	accumDuration := min(c.accumDuration, otherChange.accumDuration)
+	if otherChange.when.Sub(c.when) > accumDuration {
+		return false
+	}
+	c.forward = append(c.forward, otherChange.forward...)
+	c.reverse = append(otherChange.reverse, c.reverse...)
+	c.when = otherChange.when
+	c.accumDuration = otherChange.accumDuration
+	return true
 }
 
 // CombinedChange creates a composite change off of a number of smaller changes.
