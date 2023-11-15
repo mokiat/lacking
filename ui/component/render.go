4100c7d26dd8b025450a1b3d03549526da9604c1
Momchil Atanasov <momchil.dev@gmail.com>
2023-11-15 23:42:22 +0200

    Add Every function to co package

--- ui/component/render.go
+++ ui/component/render.go
@@ -24,15 +24,21 @@ func Schedule(scope Scope, fn func()) {
 // amount of time. The closure is guaranteed to run on the UI thread and
 // the framework ensures that the closure will not be called if the
 // component had been destroyed in the meantime.
-//
-// Normally, you would use this function within a Once block or as a result
-// of a callback.
-// Not doing so would cause the closure function to be scheduled on every
-// rendering of the component, since the framework is free to render a component
-// at any time it deems necessary.
 func After(scope Scope, duration time.Duration, fn func()) {
 	time.AfterFunc(duration, func() {
 		Schedule(scope, fn)
+	})
+}
+
+// Every will schedule a closure function to be run every interval amount of
+// time. The closure is guaranteed to run on the UI thread and
+// the framework ensures that the closure will not be called if the
+// component had been destroyed in the meantime. In fact, the closure will
+// stop being called only once the component has been destroyed.
+func Every(scope Scope, interval time.Duration, fn func()) {
+	time.AfterFunc(interval, func() {
+		fn()
+		Every(scope, interval, fn)
 	})
 }
 
