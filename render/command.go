e8d91a51348392ad8810a788d0a9981f480fbdd6
Momchil Atanasov <momchil.dev@gmail.com>
2024-01-13 17:16:56 +0200

    Remove unused uniform calls in render API

--- render/command.go
+++ render/command.go
@@ -3,10 +3,8 @@ package render
 type CommandQueue interface {
 	BindPipeline(pipeline Pipeline)
 	Uniform1f(location UniformLocation, value float32)
-	Uniform1i(location UniformLocation, value int)
 	Uniform3f(location UniformLocation, values [3]float32)
 	Uniform4f(location UniformLocation, values [4]float32)
-	UniformMatrix4f(location UniformLocation, values [16]float32)
 	UniformBufferUnit(index int, buffer Buffer)
 	UniformBufferUnitRange(index int, buffer Buffer, offset, size int)
 	TextureUnit(index int, texture Texture)
