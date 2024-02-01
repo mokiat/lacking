899dcd6b0c5bc70d6acfa3c6ede816b99861ad1f
Momchil Atanasov <momchil.dev@gmail.com>
2024-02-02 00:08:09 +0200

    Add more direct wiring of some lights to nodes

--- game/binding.go
+++ game/binding.go
@@ -19,6 +19,10 @@ func (s BodyNodeSource) ApplyTo(node *hierarchy.Node) {
 	node.SetAbsoluteMatrix(dprec.TRSMat4(translation, rotation, scale))
 }
 
+func (s BodyNodeSource) Release() {
+	s.Body.Delete()
+}
+
 type CameraNodeTarget struct {
 	Camera *graphics.Camera
 }
@@ -27,12 +31,20 @@ func (t CameraNodeTarget) ApplyFrom(node *hierarchy.Node) {
 	t.Camera.SetMatrix(node.AbsoluteMatrix())
 }
 
+func (t CameraNodeTarget) Release() {
+	t.Camera.Delete()
+}
+
 type MeshNodeTarget struct {
 	Mesh *graphics.Mesh
 }
 
 func (t MeshNodeTarget) ApplyFrom(node *hierarchy.Node) {
 	t.Mesh.SetMatrix(node.AbsoluteMatrix())
+}
+
+func (t MeshNodeTarget) Release() {
+	t.Mesh.Delete()
 }
 
 type BoneNodeTarget struct {
@@ -45,6 +57,26 @@ func (t BoneNodeTarget) ApplyFrom(node *hierarchy.Node) {
 	t.Armature.SetBone(t.BoneIndex, dtos.Mat4(matrix))
 }
 
+func (t BoneNodeTarget) Release() {
+	// Nothing to do
+}
+
+type AmbientLightNodeTarget struct {
+	Light *graphics.AmbientLight
+}
+
+func (t AmbientLightNodeTarget) ApplyFrom(node *hierarchy.Node) {
+	// TODO:
+	// matrix := node.AbsoluteMatrix()
+	// translation, rotation, _ := matrix.TRS()
+	// t.Light.SetPosition(translation)
+	// t.Light.SetRotation(rotation)
+}
+
+func (t AmbientLightNodeTarget) Release() {
+	t.Light.Delete()
+}
+
 type PointLightNodeTarget struct {
 	Light *graphics.PointLight
 }
@@ -52,6 +84,10 @@ type PointLightNodeTarget struct {
 func (t PointLightNodeTarget) ApplyFrom(node *hierarchy.Node) {
 	matrix := node.AbsoluteMatrix()
 	t.Light.SetPosition(matrix.Translation())
+}
+
+func (t PointLightNodeTarget) Release() {
+	t.Light.Delete()
 }
 
 type SpotLightNodeTarget struct {
@@ -63,6 +99,10 @@ func (t SpotLightNodeTarget) ApplyFrom(node *hierarchy.Node) {
 	translation, rotation, _ := matrix.TRS()
 	t.Light.SetPosition(translation)
 	t.Light.SetRotation(rotation)
+}
+
+func (t SpotLightNodeTarget) Release() {
+	t.Light.Delete()
 }
 
 type DirectionalLightNodeTarget struct {
@@ -84,4 +124,16 @@ func (t DirectionalLightNodeTarget) ApplyFrom(node *hierarchy.Node) {
 		t.Light.SetPosition(translation)
 		t.Light.SetRotation(rotation)
 	}
+}
+
+func (t DirectionalLightNodeTarget) Release() {
+	t.Light.Delete()
+}
+
+func DirectionalLightFromNode(node *hierarchy.Node) *graphics.DirectionalLight {
+	target, ok := node.Target().(DirectionalLightNodeTarget)
+	if !ok {
+		return nil
+	}
+	return target.Light
 }
--- game/hierarchy/binding.go
+++ game/hierarchy/binding.go
@@ -6,6 +6,10 @@ type NodeSource interface {
 
 	// ApplyTo requests that any transform be applied to the specified node.
 	ApplyTo(node *Node)
+
+	// Release indicates that the node has been deleted and the source can be
+	// deleted.
+	Release()
 }
 
 // NodeTarget represents an abstraction that is able to modify its transform
@@ -14,4 +18,8 @@ type NodeTarget interface {
 
 	// ApplyFrom requests that the node's transform be applied to the receiver.
 	ApplyFrom(node *Node)
+
+	// Release indicates that the node has been deleted and the target can be
+	// deleted.
+	Release()
 }
