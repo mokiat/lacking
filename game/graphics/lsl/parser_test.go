d4346d313f524b740bb059ee1a394ce53763bad6
Momchil Atanasov <momchil.dev@gmail.com>
2024-03-24 00:39:26 +0200

    Further WIP on parser implementation

--- game/graphics/lsl/parser_test.go
+++ game/graphics/lsl/parser_test.go
@@ -36,25 +36,22 @@ var _ = Describe("Parse", func() {
 			`
 		})
 
-		It("stores the comments", func() {
+		It("ignores the comments", func() {
 			Expect(outErr).ToNot(HaveOccurred())
-			Expect(outShader).To(Equal(&lsl.Shader{
-				Declarations: []lsl.Declaration{
-					&lsl.CommentDeclaration{
-						Comment: "This is a comment",
-					},
-				},
-			}))
+			Expect(outShader).To(Equal(&lsl.Shader{}))
 		})
 	})
 
 	When("uniform blocks are present", func() {
 		BeforeEach(func() {
 			inSource = `
-				uniform {
-					color vec3
-					intensity float
+				uniform { // header
+					color vec3 // field1
+					// has two fields
+					intensity float // field2
 				}
+
+				// comment here
 
 				uniform {
 					value vec4
@@ -94,10 +91,11 @@ var _ = Describe("Parse", func() {
 	When("varying blocks are present", func() {
 		BeforeEach(func() {
 			inSource = `
-				varying {
-					color vec3
-					intensity float
-				}
+				varying { // header
+					color vec3 // field 1
+					// two fields
+					intensity float // field2
+				} // footer
 
 				varying {
 					value vec4
@@ -134,23 +132,23 @@ var _ = Describe("Parse", func() {
 		})
 	})
 
-	When("function definitions are present", func() {
+	When("function declarations are present", func() {
 		BeforeEach(func() {
 			inSource = `
-				func Vertex(a vec3, b vec4) (float, vec2) {
+				func vertex(a vec3, b vec4) (float, vec2) {
 				}
-
-				func Fragment() {
+				
+				func #fragment() {
 				}
 			`
 		})
 
-		It("produces a shader with the function definitions", func() {
+		It("produces a shader with the function declaration", func() {
 			Expect(outErr).ToNot(HaveOccurred())
 			Expect(outShader).To(Equal(&lsl.Shader{
 				Declarations: []lsl.Declaration{
 					&lsl.FunctionDeclaration{
-						Name: "Vertex",
+						Name: "vertex",
 						Inputs: []lsl.Field{
 							{
 								Name: "a",
@@ -173,7 +171,7 @@ var _ = Describe("Parse", func() {
 						},
 					},
 					&lsl.FunctionDeclaration{
-						Name:    "Fragment",
+						Name:    "#fragment",
 						Inputs:  nil,
 						Outputs: nil,
 					},
@@ -185,10 +183,11 @@ var _ = Describe("Parse", func() {
 	When("variable declarations are present", func() {
 		BeforeEach(func() {
 			inSource = `
-			func test() {
-				var color vec3 = vec3(1.0, 0.0, 1.0)
-				var intensity float
-			}
+				func test() { // a test function
+					var color vec3 = vec3(1.0,-0.5, 0.1) // this has assignment
+					// some comment
+					var intensity float // this is just a declaration
+				} // so much for it
 			`
 		})
 
@@ -204,6 +203,20 @@ var _ = Describe("Parse", func() {
 							&lsl.VariableDeclaration{
 								Name: "color",
 								Type: lsl.TypeNameVec3,
+								Assignment: &lsl.FunctionCall{
+									Name: "vec3",
+									Arguments: []lsl.Expression{
+										&lsl.FloatLiteral{
+											Value: 1.0,
+										},
+										&lsl.FloatLiteral{
+											Value: -0.5,
+										},
+										&lsl.FloatLiteral{
+											Value: 0.1,
+										},
+									},
+								},
 							},
 							&lsl.VariableDeclaration{
 								Name: "intensity",
@@ -215,4 +228,43 @@ var _ = Describe("Parse", func() {
 			}))
 		})
 	})
+})
+
+var _ = FDescribe("Parser", func() {
+
+	DescribeTable("ParseNamedParameterList", func(inSource string, expectedFields []lsl.Field) {
+		parser := lsl.NewParser(inSource)
+		fields, err := parser.ParseNamedParameterList()
+		Expect(err).ToNot(HaveOccurred())
+		Expect(fields).To(Equal(expectedFields))
+	},
+		Entry("empty list",
+			``,
+			nil,
+		),
+		Entry("single parameter",
+			`color vec4`,
+			[]lsl.Field{
+				{Name: "color", Type: "vec4"},
+			},
+		),
+		Entry("multiple parameters, single line",
+			`color vec4, intensity float`,
+			[]lsl.Field{
+				{Name: "color", Type: "vec4"},
+				{Name: "intensity", Type: "float"},
+			},
+		),
+		Entry("multiple parameters, multiple lines",
+			`
+			color vec4, // first param here
+			// there will be a second param
+			intensity float,
+			`,
+			[]lsl.Field{
+				{Name: "color", Type: "vec4"},
+				{Name: "intensity", Type: "float"},
+			},
+		),
+	)
 })
