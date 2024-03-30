02eba4b65f17aa2d6108a5f573003e6498516082
Momchil Atanasov <momchil.dev@gmail.com>
2024-03-30 13:26:27 +0200

    Improve new asset DSL

--- game/newasset/dsl/provider.go
+++ game/newasset/dsl/provider.go
@@ -1,34 +1,41 @@
 package dsl
 
-import (
-	"sync"
-)
+import "sync"
+
+type GetFunc[T any] func() (T, error)
 
 type Provider[T any] interface {
 	Get() (T, error)
 	Digest() ([]byte, error)
 }
 
-func OnceProvider[T any](delegate Provider[T]) Provider[T] {
-	return &onceProvider[T]{
-		getFunc: sync.OnceValues(func() (T, error) {
-			return delegate.Get()
-		}),
-		digestFunc: sync.OnceValues(func() ([]byte, error) {
-			return delegate.Digest()
-		}),
+func FuncProvider[T any](get GetFunc[T], digest DigestFunc) Provider[T] {
+	return &funcProvider[T]{
+		getFunc:    get,
+		digestFunc: digest,
 	}
 }
 
-type onceProvider[T any] struct {
-	getFunc    func() (T, error)
-	digestFunc func() ([]byte, error)
+type funcProvider[T any] struct {
+	getFunc    GetFunc[T]
+	digestFunc DigestFunc
 }
 
-func (p *onceProvider[T]) Get() (T, error) {
+func (p *funcProvider[T]) Get() (T, error) {
 	return p.getFunc()
 }
 
-func (p *onceProvider[T]) Digest() ([]byte, error) {
+func (p *funcProvider[T]) Digest() ([]byte, error) {
 	return p.digestFunc()
+}
+
+func OnceProvider[T any](delegate Provider[T]) Provider[T] {
+	return FuncProvider[T](
+		sync.OnceValues(func() (T, error) {
+			return delegate.Get()
+		}),
+		sync.OnceValues(func() ([]byte, error) {
+			return delegate.Digest()
+		}),
+	)
 }
--- game/newasset/dsl/transformer.go
+++
@@ -1,6 +0,0 @@
-package dsl
-
-type Operation interface {
-	Apply(target any) error
-	Digest() ([]byte, error)
-}
