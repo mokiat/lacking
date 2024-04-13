56bd4a3770bf0ebe40e671372877220a7d55c63f
Momchil Atanasov <momchil.dev@gmail.com>
2024-04-13 19:26:12 +0300

    Improve DSL further

--- game/newasset/dsl/provider_model.go
+++ game/newasset/dsl/provider_model.go
@@ -6,26 +6,30 @@ import (
 	"github.com/mokiat/lacking/game/newasset/mdl"
 )
 
+// CreateModel creates a new model with the specified name and operations.
 func CreateModel(name string, operations ...Operation) Provider[*mdl.Model] {
-	get := func() (*mdl.Model, error) {
-		var model mdl.Model
-		model.SetName(name)
-		for _, operation := range operations {
-			if err := operation.Apply(&model); err != nil {
-				return nil, err
-			}
-		}
-		return &model, nil
-	}
-
-	digest := func() ([]byte, error) {
-		return CreateDigest("create-model", name, operations)
-	}
-
-	provider := OnceProvider(FuncProvider(get, digest))
 	if _, ok := modelProviders[name]; ok {
 		panic(fmt.Sprintf("provider for model %q already exists", name))
 	}
-	modelProviders[name] = provider
-	return provider
+
+	modelProviders[name] = OnceProvider(FuncProvider(
+		// get function
+		func() (*mdl.Model, error) {
+			var model mdl.Model
+			model.SetName(name)
+			for _, operation := range operations {
+				if err := operation.Apply(&model); err != nil {
+					return nil, err
+				}
+			}
+			return &model, nil
+		},
+
+		// digest function
+		func() ([]byte, error) {
+			return CreateDigest("create-model", name, operations)
+		},
+	))
+
+	return modelProviders[name]
 }
