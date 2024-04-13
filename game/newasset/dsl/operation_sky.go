cc7eb6cb4a68f75222ae62ec8b52f1966854b77e
Momchil Atanasov <momchil.dev@gmail.com>
2024-04-13 19:12:06 +0300

    Improve sky-related DSL

--- game/newasset/dsl/operation_sky.go
+++ game/newasset/dsl/operation_sky.go
@@ -3,26 +3,42 @@ package dsl
 import (
 	"fmt"
 
+	"github.com/mokiat/gomath/dprec"
 	"github.com/mokiat/lacking/game/newasset/mdl"
 )
 
+// AddSkyLayer adds a sky layer to the target.
 func AddSkyLayer(layerProvider Provider[mdl.SkyLayer]) Operation {
-	apply := func(target any) error {
-		sky, ok := target.(mdl.SkyLayerable)
-		if !ok {
-			return fmt.Errorf("target %T is not a sky layerable", target)
-		}
-		layer, err := layerProvider.Get()
-		if err != nil {
-			return fmt.Errorf("error getting sky layer: %w", err)
-		}
-		sky.AddSkyLayer(layer)
-		return nil
-	}
+	return FuncOperation(
+		// apply function
+		func(target any) error {
+			layer, err := layerProvider.Get()
+			if err != nil {
+				return fmt.Errorf("error getting sky layer: %w", err)
+			}
 
-	digest := func() ([]byte, error) {
-		return CreateDigest("add-sky-layer", layerProvider)
-	}
+			sky, ok := target.(mdl.SkyLayerable)
+			if !ok {
+				return fmt.Errorf("target %T is not a sky layerable", target)
+			}
+			sky.AddSkyLayer(layer)
 
-	return FuncOperation(apply, digest)
+			return nil
+		},
+
+		// digest function
+		func() ([]byte, error) {
+			return CreateDigest("add-sky-layer", layerProvider)
+		},
+	)
+}
+
+// SetSkyColor sets the color of the sky.
+func SetSkyColor(colorProvider Provider[dprec.Vec4]) Operation {
+	return SetProperty("skyColor", DPVec4ToSPVec4(colorProvider))
+}
+
+// SetSkySampler sets the sampler of the sky.
+func SetSkySampler(samplerProvider Provider[*mdl.Sampler]) Operation {
+	return SetSampler("skyColorSampler", samplerProvider)
 }
