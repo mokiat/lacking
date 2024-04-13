44fcea9f1d109518a427fdab912abd1b8da3c3a3
Momchil Atanasov <momchil.dev@gmail.com>
2024-04-13 21:18:43 +0300

    Improve cube texture construction DSL

---
+++ game/newasset/dsl/operation_texture.go
@@ -0,0 +1,37 @@
+package dsl
+
+import (
+	"fmt"
+
+	"github.com/mokiat/lacking/game/newasset/mdl"
+)
+
+// SetFormat configures the format of the target.
+func SetFormat(formatProvider Provider[mdl.TextureFormat]) Operation {
+	type formatConfigurable interface {
+		SetFormat(mdl.TextureFormat)
+	}
+
+	return FuncOperation(
+		// apply function
+		func(target any) error {
+			format, err := formatProvider.Get()
+			if err != nil {
+				return fmt.Errorf("error getting format: %w", err)
+			}
+
+			configurable, ok := target.(formatConfigurable)
+			if !ok {
+				return fmt.Errorf("target %T is not configurable with format", target)
+			}
+			configurable.SetFormat(format)
+
+			return nil
+		},
+
+		// digest function
+		func() ([]byte, error) {
+			return CreateDigest("set-format", formatProvider)
+		},
+	)
+}
