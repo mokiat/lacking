fd454b6d885325bc24d294142d50e3f8639dbfc6
Momchil Atanasov <momchil.dev@gmail.com>
2024-06-04 00:25:20 +0300

    Add ability to edit materials via DSL

--- game/asset/dsl/provider_material.go
+++ game/asset/dsl/provider_material.go
@@ -5,3 +5,43 @@ import "github.com/mokiat/lacking/game/asset/mdl"
 func CreateMaterial(name string, opts ...Operation) Provider[*mdl.Material] {
 	panic("TODO")
 }
+
+// CreateMaterialPass creates a provider that will create a material pass
+// with the specified options.
+func CreateMaterialPass(opts ...Operation) Provider[*mdl.MaterialPass] {
+	return OnceProvider(FuncProvider(
+		// get function
+		func() (*mdl.MaterialPass, error) {
+			pass := mdl.NewMaterialPass()
+			for _, opt := range opts {
+				if err := opt.Apply(pass); err != nil {
+					return nil, err
+				}
+			}
+			return pass, nil
+		},
+
+		// digest function
+		func() ([]byte, error) {
+			return CreateDigest("create-material-pass", opts)
+		},
+	))
+}
+
+// CreateShader creates a provider that will create a shader with the
+// specified type and source code.
+func CreateShader(shaderType mdl.ShaderType, code string) Provider[*mdl.Shader] {
+	return OnceProvider(FuncProvider(
+		// get function
+		func() (*mdl.Shader, error) {
+			shader := mdl.NewShader(shaderType)
+			shader.SetSourceCode(code)
+			return shader, nil
+		},
+
+		// digest function
+		func() ([]byte, error) {
+			return CreateDigest("create-shader", uint8(shaderType), code)
+		},
+	))
+}
