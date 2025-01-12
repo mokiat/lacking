eb560e8b2dcfc405101d11d18cb1611bf9164fca
Momchil Atanasov <momchil.dev@gmail.com>
2025-01-12 15:59:22 +0200

    Document LSL operators

--- docs/manual/graphics/shader.md
+++ docs/manual/graphics/shader.md
@@ -1,7 +1,75 @@
 # Shader
 
-The lacking game engine uses a custom shading language. This language is then transpiled to the respective GPU API shader language (e.g. GLSL, WGSL).
+The Lacking game engine uses a custom shading language called LSL. This language is then transpiled to the respective GPU API shader language (e.g. GLSL, WGSL).
 The syntax of the language is very similar to the Go syntax, though there are some notable differences. Using Go-like syntax allows one to write in a consistent way game code and shader code. Furthemore, the Go syntax is designed to be easily and unambiguously parsable.
+
+## Comments
+
+Comments can be added with `//` anywhere in the file, where the remainder of the line is considered a comment and is ignored.
+
+```
+// An example comment.
+```
+
+## Operators
+
+There are a number of operators supported by the LSL language.
+
+### Assignment Operators
+
+Following is a list of assignment operators that can be used to assign a value to a variable.
+
+| Operator | Description |
+| -------- | ----------- |
+| `=` | Assigns the value to the variable. Both sides need to have the same type. |
+| `:=` | Defines a new variable and initializes it with the contents and type of the value. |
+| `+=` | Adds the value to the variable. |
+| `-=` | Subtracts the value from the variable. |
+| `*=` | Multiplies the variable by the value. |
+| `/=` | Divides the variable by the value. |
+| `%=` | Assigns the modulo of the variable and the value. |
+| `<<=` | Shifts the variable to the left by the value number of bits. |
+| `>>=` | Shifts the variable to the right by the value number of bits. |
+| `&=` | Assigns the bitwise AND operation on the variable and the value to the variable. |
+| `\|=` | Assigns the bitwise OR operation on the variable and the value to the variable. |
+| `^=` | Assigns the bitwise XOR operation on the variable and the value to the variable. |
+
+### Unary Operators
+
+The following unary operators can be used inside expressions to transform a single sub-expression.
+
+| Operator | Description |
+| -------- | ----------- |
+| `!` | Inverts the value of the expression. It needs to be a boolean expression or a boolean vector expression. |
+| `-` | Negates the value of the expression. It needs to be a numeric expression or a numeric vector expression. |
+| `+` | A no-op change to the value of the expression. it needs to be a numeric expression vector expression. |
+| `^` | Performs a bitwise NOT operation on the value of the expression. It needs to be a numeric expression or a numeric vector expression. |
+
+### Binary Operators
+
+The following binary operators can be used inside expressions to combine two sub-expressions.
+
+| Operator | Description |
+| -------- | ----------- |
+| `+` | Returns the sum of the two expressions. Both sides need to be numeric expressions of the same type. |
+| `-` | Returns the difference between the two expressions. Both sides need to be numeric expressions of the same type. |
+| `*` | Returns the multiplication of the two expressions. Both sides need to be numeric expressions of the same type. |
+| `/` | Returns the division of the two expressions. Both sides need to be numeric expressions of the same type. |
+| `%` | Returns the modulo of the two expressions. Both sides need to be integer expressions of the same type. |
+| `<<` | Returns the bitwise left-shift of the left expression by the right expression. Both sides need to be integer expressions. |
+| `>>` | Returns the bitwise right-shift of the left expression by the right expression. Both sides need to be integer expressions. |
+| `==` | Returns a boolean value indicating whether the two expressions are equal. Both sides need to be expressions of the same type and comparable. |
+| `!=` | Returns a boolean value indicating whether the two expressions are different. Both sides need to be expressions of the same type and comparable. |
+| `<` | Returns a boolean value indicating whether the first expression is smaller than the second expression. Both sides need to be expressions of the same type and be ordered. |
+| `>` | Returns a boolean value indicating whether the second expression is smaller than the first expression. Both sides need to be expressions of the same type and be ordered. |
+| `<=` | Returns a boolean value indicating whether the first expression is smaller than or equal to the second expression. Both sides need to be expressions of the same type and be ordered. |
+| `>=` | Returns a boolean value indicating whether the second expression is smaller than or equal to the first expression. Both sides need to be expressions of the same type and be ordered. |
+| `&` | Returns the result of a bitwise AND operation on the two expressions. Both sides need to be integer expressions of the same type. |
+| `\|` | Returns the result of a bitwise OR operation on the two expressions. Both sides need to be integer expressions of the same type. |
+| `^` | Returns the result of a bitwise XOR operation on the two expressions. Both sides need to be integer expressions of the same type. |
+| `&&` | Returns the result of a logical AND operation on the two expressions. Both sides need to be boolean expressions of the same type. |
+| `\|\|` | Returns the result of a logical OR operation on the two expressions. Both sides need to be boolean expressions of the same type. |
+
 
 ## Types
 
--- game/graphics/lsl/spec.go
+++ game/graphics/lsl/spec.go
@@ -8,18 +8,53 @@ var (
 )
 
 const (
-	AssignmentOperatorEq   = "="
+	// AssignmentOperatorEq is the assignment operator "=". It assigns the
+	// value to the variable.
+	AssignmentOperatorEq = "="
+
+	// AssignmentOperatorAuto is the assignment operator ":=", where the type
+	// of the variable is inferred from the value.
 	AssignmentOperatorAuto = ":="
-	AssignmentOperatorAdd  = "+="
-	AssignmentOperatorSub  = "-="
-	AssignmentOperatorMul  = "*="
-	AssignmentOperatorDiv  = "/="
-	AssignmentOperatorMod  = "%="
-	AssignmentOperatorShl  = "<<="
-	AssignmentOperatorShr  = ">>="
-	AssignmentOperatorAnd  = "&="
-	AssignmentOperatorOr   = "|="
-	AssignmentOperatorXor  = "^="
+
+	// AssignmentOperatorAdd is the assignment operator "+=". It adds the value
+	// to the variable.
+	AssignmentOperatorAdd = "+="
+
+	// AssignmentOperatorSub is the assignment operator "-=". It subtracts the
+	// value from the variable.
+	AssignmentOperatorSub = "-="
+
+	// AssignmentOperatorMul is the assignment operator "*=". It multiplies the
+	// variable by the value.
+	AssignmentOperatorMul = "*="
+
+	// AssignmentOperatorDiv is the assignment operator "/=". It divides the
+	// variable by the value.
+	AssignmentOperatorDiv = "/="
+
+	// AssignmentOperatorMod is the assignment operator "%=". It takes the
+	// modulo of the variable and the value.
+	AssignmentOperatorMod = "%="
+
+	// AssignmentOperatorShl is the assignment operator "<<=". It shifts the
+	// variable to the left by the value.
+	AssignmentOperatorShl = "<<="
+
+	// AssignmentOperatorShr is the assignment operator ">>=". It shifts the
+	// variable to the right by the value.
+	AssignmentOperatorShr = ">>="
+
+	// AssignmentOperatorAnd is the assignment operator "&=". It performs a
+	// bitwise AND operation on the variable and the value.
+	AssignmentOperatorAnd = "&="
+
+	// AssignmentOperatorOr is the assignment operator "|=". It performs a
+	// bitwise OR operation on the variable and the value.
+	AssignmentOperatorOr = "|="
+
+	// AssignmentOperatorXor is the assignment operator "^=". It performs a
+	// bitwise XOR operation on the variable and the value.
+	AssignmentOperatorXor = "^="
 )
 
 var assignmentOperators = []string{
@@ -38,9 +73,17 @@ var assignmentOperators = []string{
 }
 
 const (
-	UnaryOperatorNot    = "!"
-	UnaryOperatorNeg    = "-"
-	UnaryOperatorPos    = "+"
+	// UnaryOperatorNot is the unary operator "!". It inverts the value.
+	UnaryOperatorNot = "!"
+
+	// UnaryOperatorNeg is the unary operator "-". It negates the value.
+	UnaryOperatorNeg = "-"
+
+	// UnaryOperatorPos is the unary operator "+". It is a no-op.
+	UnaryOperatorPos = "+"
+
+	// UnaryOperatorBitNot is the unary operator "^". It performs a bitwise
+	// NOT operation on the value.
 	UnaryOperatorBitNot = "^"
 )
 
@@ -52,24 +95,73 @@ var unaryOperators = []string{
 }
 
 const (
-	BinaryOperatorAdd       = "+"
-	BinaryOperatorSub       = "-"
-	BinaryOperatorMul       = "*"
-	BinaryOperatorDiv       = "/"
-	BinaryOperatorMod       = "%"
-	BinaryOperatorShl       = "<<"
-	BinaryOperatorShr       = ">>"
-	BinaryOperatorEq        = "=="
-	BinaryOperatorNotEq     = "!="
-	BinaryOperatorLess      = "<"
-	BinaryOperatorGreater   = ">"
-	BinaryOperatorLessEq    = "<="
+	// BinaryOperatorAdd is the binary operator "+". It adds two values.
+	BinaryOperatorAdd = "+"
+
+	// BinaryOperatorSub is the binary operator "-". It subtracts two values.
+	BinaryOperatorSub = "-"
+
+	// BinaryOperatorMul is the binary operator "*". It multiplies two values.
+	BinaryOperatorMul = "*"
+
+	// BinaryOperatorDiv is the binary operator "/". It divides two values.
+	BinaryOperatorDiv = "/"
+
+	// BinaryOperatorMod is the binary operator "%". It takes the modulo of two
+	// values.
+	BinaryOperatorMod = "%"
+
+	// BinaryOperatorShl is the binary operator "<<". It shifts the first value
+	// to the left by the second value.
+	BinaryOperatorShl = "<<"
+
+	// BinaryOperatorShr is the binary operator ">>". It shifts the first value
+	// to the right by the second value.
+	BinaryOperatorShr = ">>"
+
+	// BinaryOperatorEq is the binary operator "==". It checks if two values are
+	// equal.
+	BinaryOperatorEq = "=="
+
+	// BinaryOperatorNotEq is the binary operator "!=". It checks if two values
+	// are not equal.
+	BinaryOperatorNotEq = "!="
+
+	// BinaryOperatorLess is the binary operator "<". It checks if the first
+	// value is less than the second value.
+	BinaryOperatorLess = "<"
+
+	// BinaryOperatorGreater is the binary operator ">". It checks if the first
+	// value is greater than the second value.
+	BinaryOperatorGreater = ">"
+
+	// BinaryOperatorLessEq is the binary operator "<=". It checks if the first
+	// value is less than or equal to the second value.
+	BinaryOperatorLessEq = "<="
+
+	// BinaryOperatorGreaterEq is the binary operator ">=". It checks if the
+	// first value is greater than or equal to the second value.
 	BinaryOperatorGreaterEq = ">="
-	BinaryOperatorBitAnd    = "&"
-	BinaryOperatorBitOr     = "|"
-	BinaryOperatorBitXor    = "^"
-	BinaryOperatorAnd       = "&&"
-	BinaryOperatorOr        = "||"
+
+	// BinaryOperatorBitAnd is the binary operator "&". It performs a bitwise
+	// AND operation on two values.
+	BinaryOperatorBitAnd = "&"
+
+	// BinaryOperatorBitOr is the binary operator "|". It performs a bitwise OR
+	// operation on two values.
+	BinaryOperatorBitOr = "|"
+
+	// BinaryOperatorBitXor is the binary operator "^". It performs a bitwise
+	// XOR operation on two values.
+	BinaryOperatorBitXor = "^"
+
+	// BinaryOperatorAnd is the binary operator "&&". It performs a logical AND
+	// operation on two values.
+	BinaryOperatorAnd = "&&"
+
+	// BinaryOperatorOr is the binary operator "||". It performs a logical OR
+	// operation on two values.
+	BinaryOperatorOr = "||"
 )
 
 var binaryOperators = []string{
