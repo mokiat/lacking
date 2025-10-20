070b655323865cda7b6d1514523fd01853315498
Momchil Atanasov <momchil.dev@gmail.com>
2025-10-20 17:34:07 +0300

    Implement circle-polygon intersection

--- util/shape2d/intersection_circle_polygon.go
+++ util/shape2d/intersection_circle_polygon.go
@@ -1,6 +1,16 @@
 package shape2d
 
+// CheckCirclePolygonIntersection checks for intersection between a circle and
+// a polygon.
 func CheckCirclePolygonIntersection(circle Circle, polygon Polygon) (Intersection, bool) {
-	// TODO: Implement
-	return Intersection{}, false
+	var collection LargestIntersection
+	for _, edge := range polygon.Edges {
+		if !IsCircleCircleIntersection(circle, edge.BoundingCircle()) {
+			continue
+		}
+		if intersection, ok := CheckCircleEdgeIntersection(circle, edge); ok {
+			collection.AddIntersection(intersection)
+		}
+	}
+	return collection.Intersection()
 }
