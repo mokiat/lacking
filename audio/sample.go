4cb16beb4059c005b0d7bffdce8d1f6a3a4657f9
Momchil Atanasov <momchil.dev@gmail.com>
2026-02-10 19:00:27 +0200

    Add audio sample utilities

--- audio/sample.go
+++ audio/sample.go
@@ -1,5 +1,11 @@
 package audio
 
+import (
+	"math"
+
+	"github.com/mokiat/gomath/sprec"
+)
+
 // Sample represents a single audio sample with left and right channel data.
 type Sample struct {
 
@@ -8,4 +14,46 @@ type Sample struct {
 
 	// Right is the right channel sample value.
 	Right float32
+}
+
+// SampleCount calculates the number of samples for a given duration in seconds
+// given the used sample rate.
+func SampleCount(seconds float32, sampleRate int) int {
+	return int(float32(sampleRate) * seconds)
+}
+
+// Resample resamples the given audio samples from one sample rate to another.
+func Resample(samples []Sample, fromRate int, toRate int) []Sample {
+	if (fromRate == toRate) || (len(samples) == 0) {
+		return samples
+	}
+	if fromRate <= 0 || toRate <= 0 {
+		panic("invalid sample rate")
+	}
+	oldLength := len(samples)
+
+	scale := float64(toRate) / float64(fromRate)
+	newLength := int(float64(len(samples))*scale + 0.5)
+	if newLength <= 0 {
+		return nil
+	}
+
+	result := make([]Sample, newLength)
+	step := float64(oldLength-1) / float64(newLength-1)
+	for i := range newLength {
+		srcPosition, srcFraction := math.Modf(float64(i) * step)
+		srcIndexPrev := min(int(srcPosition), oldLength-1)
+		srcIndexNext := min(srcIndexPrev+1, oldLength-1)
+		if srcIndexPrev == srcIndexNext {
+			result[i] = samples[srcIndexPrev]
+		} else {
+			prev := samples[srcIndexPrev]
+			next := samples[srcIndexNext]
+			result[i] = Sample{
+				Left:  sprec.Mix(prev.Left, next.Left, float32(srcFraction)),
+				Right: sprec.Mix(prev.Right, next.Right, float32(srcFraction)),
+			}
+		}
+	}
+	return result
 }
