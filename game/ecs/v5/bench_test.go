11056a41b64163bf63b759a15000928835539c30
Momchil Atanasov <momchil.dev@gmail.com>
2026-05-08 22:09:07 +0300

    Add query benchmarks

--- game/ecs/v5/bench_test.go
+++ game/ecs/v5/bench_test.go
@@ -87,3 +87,98 @@ func BenchmarkEditEntity(b *testing.B) {
 		})
 	}
 }
+
+func BenchmarkQueryEntities(b *testing.B) {
+	type Position struct {
+		X, Y float64
+	}
+	type Velocity struct {
+		X, Y float64
+	}
+
+	const entityCount = 1000
+
+	scope := ecs.NewScope()
+	positionType := ecs.Type[Position](scope)
+	velocityType := ecs.Type[Velocity](scope)
+	scene := ecs.NewScene(scope)
+
+	for range entityCount {
+		id := scene.CreateEntity()
+		scene.EditEntity(id, func(op *ecs.EditOperation) {
+			ecs.AddComponent(op, positionType, Position{X: 1.0, Y: 2.0})
+			ecs.AddComponent(op, velocityType, Velocity{X: 0.5, Y: 0.5})
+		})
+	}
+
+	for b.Loop() {
+		count := 0
+		scene.QueryEntities(
+			ecs.Conditions(
+				ecs.HasComponent(positionType),
+			),
+			func(_ ecs.ID, op *ecs.ReadOperation) bool {
+				pos := ecs.GetComponent(op, positionType)
+				vel := ecs.GetComponent(op, velocityType)
+				pos.X += vel.X
+				pos.Y += vel.Y
+				count++
+				return true
+			},
+		)
+		if count != entityCount {
+			b.Fatalf("unexpected entity count: got %d, want %d", count, entityCount)
+		}
+	}
+}
+
+func BenchmarkQueryEntitiesMultiArchetype(b *testing.B) {
+	type Position struct {
+		X, Y float64
+	}
+	type Velocity struct {
+		X, Y float64
+	}
+	type Tag struct{}
+
+	const entityCount = 1000
+
+	scope := ecs.NewScope()
+	positionType := ecs.Type[Position](scope)
+	velocityType := ecs.Type[Velocity](scope)
+	tagType := ecs.Type[Tag](scope)
+	scene := ecs.NewScene(scope)
+
+	for i := range entityCount {
+		id := scene.CreateEntity()
+		scene.EditEntity(id, func(op *ecs.EditOperation) {
+			ecs.AddComponent(op, positionType, Position{X: 1.0, Y: 2.0})
+			ecs.AddComponent(op, velocityType, Velocity{X: 0.5, Y: 0.5})
+			if i%2 == 0 {
+				ecs.AddComponent(op, tagType, Tag{})
+			}
+		})
+	}
+
+	for b.Loop() {
+		count := 0
+		scene.QueryEntities(
+			ecs.Conditions(
+				ecs.HasComponent(positionType),
+				ecs.HasComponent(velocityType),
+				ecs.HasComponent(tagType),
+			),
+			func(_ ecs.ID, op *ecs.ReadOperation) bool {
+				pos := ecs.GetComponent(op, positionType)
+				vel := ecs.GetComponent(op, velocityType)
+				pos.X += vel.X
+				pos.Y += vel.Y
+				count++
+				return true
+			},
+		)
+		if count != entityCount/2 {
+			b.Fatalf("unexpected entity count: got %d, want %d", count, entityCount/2)
+		}
+	}
+}
