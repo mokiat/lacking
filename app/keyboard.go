1fdaf04edb240a47e4a69b1a6ac0b9478eb60668
Momchil Atanasov <momchil.dev@gmail.com>
2026-05-10 21:51:54 +0300

    app: document key codes

--- app/keyboard.go
+++ app/keyboard.go
@@ -64,88 +64,171 @@ func (a KeyboardAction) String() string {
 }
 
 const (
+	// KeyCodeEscape indicates the Escape key.
 	KeyCodeEscape KeyCode = 1 + iota
+	// KeyCodeEnter indicates the Enter key.
 	KeyCodeEnter
+	// KeyCodeSpace indicates the Space key.
 	KeyCodeSpace
+	// KeyCodeTab indicates the Tab key.
 	KeyCodeTab
+	// KeyCodeCaps indicates the Caps Lock key.
 	KeyCodeCaps
+	// KeyCodeLeftShift indicates the left Shift key.
 	KeyCodeLeftShift
+	// KeyCodeRightShift indicates the right Shift key.
 	KeyCodeRightShift
+	// KeyCodeLeftControl indicates the left Control key.
 	KeyCodeLeftControl
+	// KeyCodeRightControl indicates the right Control key.
 	KeyCodeRightControl
+	// KeyCodeLeftAlt indicates the left Alt key.
 	KeyCodeLeftAlt
+	// KeyCodeRightAlt indicates the right Alt key.
 	KeyCodeRightAlt
+	// KeyCodeLeftSuper indicates the left Super (Win/Cmd) key.
 	KeyCodeLeftSuper
+	// KeyCodeRightSuper indicates the right Super (Win/Cmd) key.
 	KeyCodeRightSuper
+	// KeyCodeBackspace indicates the Backspace key.
 	KeyCodeBackspace
+	// KeyCodeInsert indicates the Insert key.
 	KeyCodeInsert
+	// KeyCodeDelete indicates the Delete key.
 	KeyCodeDelete
+	// KeyCodeHome indicates the Home key.
 	KeyCodeHome
+	// KeyCodeEnd indicates the End key.
 	KeyCodeEnd
+	// KeyCodePageUp indicates the Page Up key.
 	KeyCodePageUp
+	// KeyCodePageDown indicates the Page Down key.
 	KeyCodePageDown
+	// KeyCodeArrowLeft indicates the left arrow key.
 	KeyCodeArrowLeft
+	// KeyCodeArrowRight indicates the right arrow key.
 	KeyCodeArrowRight
+	// KeyCodeArrowUp indicates the up arrow key.
 	KeyCodeArrowUp
+	// KeyCodeArrowDown indicates the down arrow key.
 	KeyCodeArrowDown
+	// KeyCodeMinus indicates the minus/hyphen key.
 	KeyCodeMinus
+	// KeyCodeEqual indicates the equal sign key.
 	KeyCodeEqual
+	// KeyCodeLeftBracket indicates the left square bracket key.
 	KeyCodeLeftBracket
+	// KeyCodeRightBracket indicates the right square bracket key.
 	KeyCodeRightBracket
+	// KeyCodeSemicolon indicates the semicolon key.
 	KeyCodeSemicolon
+	// KeyCodeComma indicates the comma key.
 	KeyCodeComma
+	// KeyCodePeriod indicates the period/full stop key.
 	KeyCodePeriod
+	// KeyCodeSlash indicates the forward slash key.
 	KeyCodeSlash
+	// KeyCodeBackslash indicates the backslash key.
 	KeyCodeBackslash
+	// KeyCodeApostrophe indicates the apostrophe/single-quote key.
 	KeyCodeApostrophe
+	// KeyCodeGraveAccent indicates the grave accent/backtick key.
 	KeyCodeGraveAccent
+	// KeyCodeA indicates the A key.
 	KeyCodeA
+	// KeyCodeB indicates the B key.
 	KeyCodeB
+	// KeyCodeC indicates the C key.
 	KeyCodeC
+	// KeyCodeD indicates the D key.
 	KeyCodeD
+	// KeyCodeE indicates the E key.
 	KeyCodeE
+	// KeyCodeF indicates the F key.
 	KeyCodeF
+	// KeyCodeG indicates the G key.
 	KeyCodeG
+	// KeyCodeH indicates the H key.
 	KeyCodeH
+	// KeyCodeI indicates the I key.
 	KeyCodeI
+	// KeyCodeJ indicates the J key.
 	KeyCodeJ
+	// KeyCodeK indicates the K key.
 	KeyCodeK
+	// KeyCodeL indicates the L key.
 	KeyCodeL
+	// KeyCodeM indicates the M key.
 	KeyCodeM
+	// KeyCodeN indicates the N key.
 	KeyCodeN
+	// KeyCodeO indicates the O key.
 	KeyCodeO
+	// KeyCodeP indicates the P key.
 	KeyCodeP
+	// KeyCodeQ indicates the Q key.
 	KeyCodeQ
+	// KeyCodeR indicates the R key.
 	KeyCodeR
+	// KeyCodeS indicates the S key.
 	KeyCodeS
+	// KeyCodeT indicates the T key.
 	KeyCodeT
+	// KeyCodeU indicates the U key.
 	KeyCodeU
+	// KeyCodeV indicates the V key.
 	KeyCodeV
+	// KeyCodeW indicates the W key.
 	KeyCodeW
+	// KeyCodeX indicates the X key.
 	KeyCodeX
+	// KeyCodeY indicates the Y key.
 	KeyCodeY
+	// KeyCodeZ indicates the Z key.
 	KeyCodeZ
+	// KeyCode0 indicates the 0 key.
 	KeyCode0
+	// KeyCode1 indicates the 1 key.
 	KeyCode1
+	// KeyCode2 indicates the 2 key.
 	KeyCode2
+	// KeyCode3 indicates the 3 key.
 	KeyCode3
+	// KeyCode4 indicates the 4 key.
 	KeyCode4
+	// KeyCode5 indicates the 5 key.
 	KeyCode5
+	// KeyCode6 indicates the 6 key.
 	KeyCode6
+	// KeyCode7 indicates the 7 key.
 	KeyCode7
+	// KeyCode8 indicates the 8 key.
 	KeyCode8
+	// KeyCode9 indicates the 9 key.
 	KeyCode9
+	// KeyCodeF1 indicates the F1 function key.
 	KeyCodeF1
+	// KeyCodeF2 indicates the F2 function key.
 	KeyCodeF2
+	// KeyCodeF3 indicates the F3 function key.
 	KeyCodeF3
+	// KeyCodeF4 indicates the F4 function key.
 	KeyCodeF4
+	// KeyCodeF5 indicates the F5 function key.
 	KeyCodeF5
+	// KeyCodeF6 indicates the F6 function key.
 	KeyCodeF6
+	// KeyCodeF7 indicates the F7 function key.
 	KeyCodeF7
+	// KeyCodeF8 indicates the F8 function key.
 	KeyCodeF8
+	// KeyCodeF9 indicates the F9 function key.
 	KeyCodeF9
+	// KeyCodeF10 indicates the F10 function key.
 	KeyCodeF10
+	// KeyCodeF11 indicates the F11 function key.
 	KeyCodeF11
+	// KeyCodeF12 indicates the F12 function key.
 	KeyCodeF12
 )
 
