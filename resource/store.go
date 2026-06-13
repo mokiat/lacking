405ff872d06639551a394ccf80db7404741e927f
Momchil Atanasov <momchil.dev@gmail.com>
2026-06-14 01:04:17 +0300

    doc(resource): godoc fixes and improvements

--- resource/store.go
+++ resource/store.go
@@ -5,20 +5,22 @@ import "io"
 // Store represents a resource store that can manage multiple resources.
 type Store interface {
 
-	// Create opens a writer for the data of the specified asset.
+	// Create opens a writer for the data of the specified resource.
 	//
 	// If the operation is not supported, an errors.ErrUnsupported is returned.
 	Create(path string) (io.WriteCloser, error)
 
-	// Open opens a reader for the data of the specified asset.
+	// Open opens a reader for the data of the specified resource.
+	//
+	// If the resource does not exist, an ErrNotFound error is returned.
 	Open(path string) (io.ReadCloser, error)
 
-	// List returns all available assets.
+	// List returns all available resources.
 	//
 	// If the operation is not supported, an errors.ErrUnsupported is returned.
 	List() ([]string, error)
 
-	// Delete removes the specified asset.
+	// Delete removes the specified resource.
 	//
 	// If the operation is not supported, an errors.ErrUnsupported is returned.
 	Delete(path string) error
