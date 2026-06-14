76938bbd37ea75bd75b1eedd36cdac47f45fd3db
Momchil Atanasov <momchil.dev@gmail.com>
2026-06-15 00:13:31 +0300

    feat(shape2d): improvements and tests to circle-polygon intersection

--- util/shape2d/intersection_circle_polygon.go
+++ util/shape2d/intersection_circle_polygon.go
@@ -1,5 +1,21 @@
 package shape2d
 
+// IsCirclePolygonIntersection checks if a circle intersects with a polygon.
+//
+// Only a bool result is returned and no collision points or separation
+// normals are evaluated.
+func IsCirclePolygonIntersection(circle Circle, polygon Polygon) bool {
+	for _, edge := range polygon.Edges {
+		if !IsCircleCircleIntersection(circle, edge.BoundingCircle()) {
+			continue
+		}
+		if IsCircleEdgeIntersection(circle, edge) {
+			return true
+		}
+	}
+	return false
+}
+
 // CheckCirclePolygonIntersection checks for intersection between a circle and
 // a polygon.
 func CheckCirclePolygonIntersection(circle Circle, polygon Polygon, yield IntersectionYieldFunc) {
